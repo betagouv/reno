b4c4ab1642c2f98d57a588da4a0a007bd91f6510
Mael <laem@kont.me>
2023-12-05 12:02:41 +0100

    Police marianne

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,43 @@
+import './globals.css'
 import StyledComponentsRegistry from '../lib/registry'
+
+import localFont from 'next/font/local'
+
+const marianne = localFont({
+  src: [
+    {
+      path: '../fonts/Marianne-Thin.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Marianne-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Marianne-Regular.woff2',
+      weight: 'normal',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Marianne-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Marianne-Bold.woff2',
+      weight: 'bold',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Marianne-ExtraBold.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-marianne',
+})
 
 export default function RootLayout({
   children,
@@ -7,7 +46,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr">
-      <body>
+      <body className={marianne.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
