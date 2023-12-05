c7aa458bcf9100af4bb3dd72d005859d970cf6dd
Mael <laem@kont.me>
2023-12-05 16:48:28 +0100

    Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import css from '@/components/css/convertToJs'
 import Form from './Form'
+import logo from './icon.svg'
+import Image from 'next/image'
 
 export default function Page({ searchParams }) {
   return (
@@ -11,7 +13,24 @@ export default function Page({ searchParams }) {
         margin: 0 auto;
       `}
     >
-      <h1>Mes Aides Rénovation 2024</h1>
+      <header
+        style={css`
+          display: flex;
+          align-items: center;
+        `}
+      >
+        <Image
+          src={logo}
+          alt="Logo Mes Aides Rénovation 2024"
+          style={css`
+            width: 2rem;
+            margin-right: 0.6rem;
+            height: auto;
+            padding-top: 0.5rem;
+          `}
+        />
+        <h1>Mes Aides Rénovation&nbsp;2024</h1>
+      </header>
       <p>
         Ce petit vous aide à calculer les aides à la rénovation de votre
         logement.
