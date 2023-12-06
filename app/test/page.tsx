3839d3a50fbedfed99d53d02ee1feed8eeaefc40
Mael <laem@kont.me>
2023-12-06 17:37:10 +0100

    Test d'affichage de DPE

---
+++ app/test/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import 'dpeges/dist/style.css'
+import dpeGes from 'dpeges'
+import { useEffect, useRef } from 'react'
+
+// Voir aussi ce paquet https://github.com/aurelien-f/dpe-generator-js/tree/main
+
+export default function Test() {
+  const dpeRef = useRef(null)
+
+  useEffect(() => {
+    if (!dpeRef?.current) return
+    if (dpeRef.current.innerHTML.includes('dpeGes-bars-container')) return
+
+    dpeGes({
+      containerId: 'dpe',
+      dpeValue: 56,
+      gesValue: 98,
+      stickerHeight: 600,
+    })
+  }, [dpeRef])
+
+  return (
+    <div>
+      <div id="dpe" ref={dpeRef}></div>
+    </div>
+  )
+}
