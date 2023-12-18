21f3a3ceb9ba034253d9c917ed5b75e47a3a7787
Mael <laem@kont.me>
2023-12-18 11:15:39 +0100

    Opacité et couleur pour l'applicabilité

--- components/Result.tsx
+++ components/Result.tsx
@@ -1,16 +1,17 @@
 import css from '@/components/css/convertToJs'
 import { formatValue } from '@/node_modules/publicodes/dist/index'
 import { styled } from 'styled-components'
+
 export default function Result({ engine, isFinal, rules, dottedName }) {
   const rule = rules[dottedName]
   const evaluation = engine.evaluate(dottedName)
 
   const value = formatValue(evaluation)
+  const notApplicable = value === 'Non applicable'
 
-  console.log(dottedName, evaluation)
   return (
     <div
-      style={css`
+      style={css(`
         padding: 1.2vh;
         margin: 0 auto;
         width: 14rem;
@@ -22,7 +23,8 @@ export default function Result({ engine, isFinal, rules, dottedName }) {
           rgba(0, 0, 0, 0.12) 0px 1px 1px 0px,
           rgba(61, 59, 53, 0.16) 0px 0px 0px 1px,
           rgba(61, 59, 53, 0.08) 0px 2px 5px 0px;
-      `}
+        ${notApplicable ? 'opacity: .7;' : ''}
+      `)}
     >
       <h3
         style={css`
@@ -33,7 +35,15 @@ export default function Result({ engine, isFinal, rules, dottedName }) {
       >
         {rule.titre}
       </h3>
-      <strong>{value}</strong>
+      <strong
+        style={css(`
+		padding: 0 .2rem;
+	  ${notApplicable ? 'background: salmon; color: white' : ''}
+
+			  `)}
+      >
+        {value}
+      </strong>
       <div>Estimation {!isFinal ? '' : ' finale'}&nbsp;</div>
     </div>
   )
