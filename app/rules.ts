15a4d75fe14ae2dc4aee6e6a1e579a1e6b1bc60a
Mael <laem@kont.me>
2024-01-03 11:04:23 +0100

    Déplacement des revenus dans un nouveau fichier

--- app/rules.ts
+++ app/rules.ts
@@ -1,10 +1,11 @@
 import index from './règles/index.yaml'
+import revenus from './règles/revenus.yaml'
 import gestes from './règles/gestes.yaml'
 
 const prefix = (rules) =>
   Object.fromEntries(
     Object.entries(rules).map(([k, v]) => ['gestes . ' + k, v]),
   )
-const rules = { ...index, ...prefix(gestes) }
+const rules = { ...index, ...revenus, ...prefix(gestes) }
 
 export default rules
