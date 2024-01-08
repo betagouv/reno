a017b9f18505fae1c68988dec112f627e8262c42
Mael <laem@kont.me>
2024-01-08 09:39:41 +0100

    Ébauche de nouveau DPE, c'est assez simple en fait !

---
+++ app/dpe/page.tsx
@@ -0,0 +1,4 @@
+import DPE from '@/components/DPE2.tsx'
+export default function () {
+  return <DPE letter="F" newLetter="C" />
+}
--- components/DPE2.tsx
+++ components/DPE2.tsx
@@ -1,1 +1,50 @@
+'use client'
+import { styled } from 'styled-components'
+import data from './DPE.yaml'
+export default function DPE({ letter, newLetter }) {
+  /*
+		 *
+- énergie: 420
+  climat: 100
+  couleur: '#d7221f'
+  lettre: G
+		 * */
 
+  const found = data.find((el) => el.letter === letter)
+  return (
+    <Bars>
+      <ul>
+        {data.map((el, index) => (
+          <li key={el.lettre}>
+            <Bar
+              $background={el.couleur}
+              $index={index}
+              $selected={el.lettre === letter}
+            >
+              {el.lettre}
+            </Bar>
+          </li>
+        ))}
+      </ul>
+    </Bars>
+  )
+}
+
+const Bars = styled.div`
+  ul {
+    list-style-type: none;
+  }
+`
+const Bar = styled.div`
+  background: ${(p) => p.$background};
+  width: ${(p) => 3 + p.$index * 1.5}rem;
+  margin: 0.4rem 0;
+  padding-left: 0.6rem;
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  color: white;
+  font-weight: bold;
+  height: 2.2rem;
+  font-size: 150%;
+  border: 3px solid ${(p) => (p.$selected ? 'black' : 'transparent')};
+`
