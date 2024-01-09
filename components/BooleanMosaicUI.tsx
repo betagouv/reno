c6738bb65b9949224244428c14128c44dc925480
Mael <laem@kont.me>
2024-01-09 14:01:09 +0100

    Réduction de la taille des labels des gestes

--- components/BooleanMosaicUI.tsx
+++ components/BooleanMosaicUI.tsx
@@ -3,12 +3,15 @@
 import styled from 'styled-components'
 
 export const Fieldset = styled.fieldset`
+  ul {
+    padding-left: 1vw;
+  }
   li {
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
     label {
-      margin: 0 1rem;
+      margin: 0 0.4rem;
       whitespace: nowrap;
     }
   }
