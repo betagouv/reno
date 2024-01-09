cf687594e38c989227f6678746ef39fdae659a80
Mael <laem@kont.me>
2024-01-09 13:43:21 +0100

    Composant oublié

---
+++ components/DPELabel.tsx
@@ -0,0 +1,19 @@
+import dpeData from '@/components/DPE.yaml'
+
+import css from './css/convertToJs'
+export default function DPELabel({ index }) {
+  const { couleur, lettre } = dpeData[+index]
+  return (
+    <span
+      style={css(`
+        background: ${couleur};
+		width: 1.6rem; 
+		padding: 0 .4rem;
+		color: white;
+		border-radius: .3rem;
+      `)}
+    >
+      {lettre}
+    </span>
+  )
+}
