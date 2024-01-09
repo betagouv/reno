06c788a2c273acda1dbabc0abacd71c218d229ef
Mael <laem@kont.me>
2024-01-09 12:18:38 +0100

    Autre style pour les cartes, piqué de annuaire entreprises

--- components/UI.tsx
+++ components/UI.tsx
@@ -4,11 +4,8 @@ import styled from 'styled-components'
 export const Card = styled.div`
   margin: 0.6rem 0;
   padding: 0.2rem 1vw;
-  background: ${(p) => p.$background};
-  box-shadow:
-    rgba(0, 0, 0, 0.12) 0px 1px 1px 0px,
-    rgba(61, 59, 53, 0.16) 0px 0px 0px 1px,
-    rgba(61, 59, 53, 0.08) 0px 2px 5px 0px;
+  border: 2px solid #dfdff1;
+  border-radius: 0.3rem;
 `
 
 export const FooterWrapper = styled.footer`
