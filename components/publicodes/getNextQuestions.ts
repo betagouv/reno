2637e0bba16edcfd71f7c1b50f50a2ab3ec27a94
Mael <laem@kont.me>
2024-01-16 17:04:43 +0100

    :bug: bug probable dans l'ordre artificiel des questions

--- components/publicodes/getNextQuestions.ts
+++ components/publicodes/getNextQuestions.ts
@@ -32,13 +32,15 @@ export default function getNextQuestions(
       }),
     orderedEntries = sortBy(([k, v]) => v)(missingEntries).reverse(),
     firstEntry = orderedEntries[0],
-    maxScore = firstEntry ? [1] : 0,
-    prio = questionsConfig.prioritaires || [],
-    artificialOrdered = sortBy(([k, v]) =>
-      prio.includes(k)
-        ? maxScore + [...prio].reverse().findIndex((kk) => kk === k) + 1
-        : v,
-    )(orderedEntries).reverse()
+    maxScore = firstEntry ? firstEntry[1] : 0,
+    prio = questionsConfig.prioritaires || []
+
+  console.log({ orderedEntries }, maxScore)
+  const artificialOrdered = sortBy(([k, v]) =>
+    prio.includes(k)
+      ? maxScore + [...prio].reverse().findIndex((kk) => kk === k) + 1
+      : v,
+  )(orderedEntries).reverse()
 
   const nextQuestions = artificialOrdered.map(([k, v]) => k)
 
