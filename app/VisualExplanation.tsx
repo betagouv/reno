da165e4b296df9b363375183566f2e1aa56d4008
Mael <laem@kont.me>
2024-01-16 11:10:48 +0100

    Fichier manquant

---
+++ app/VisualExplanation.tsx
@@ -0,0 +1,74 @@
+'use client'
+import styled from 'styled-components'
+import MpaIcon from '@/public/mpa.svg'
+import MpgIcon from '@/public/mpg.svg'
+import Image from 'next/image'
+import rules from '@/app/règles/rules'
+
+export default function VisualExplanation() {
+  return (
+    <Images>
+      <li>
+        <h3>
+          Ma Prime Rénov' <strong>accompagnée</strong>
+        </h3>
+        <Image
+          src={MpaIcon}
+          alt="Illustration du parcours Ma Prime Rénov' accompagnée"
+        />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: rules['MPR . accompagnée'].descriptionHtml,
+          }}
+        />
+      </li>
+      <li>
+        <h3>
+          Ma Prime Rénov' <strong>par geste</strong>
+        </h3>
+        <Image
+          src={MpgIcon}
+          alt="Illustration du parcours Ma Prime Rénov' par gestes"
+        />
+        <div
+          dangerouslySetInnerHTML={{
+            __html: rules['MPR . non accompagnée'].descriptionHtml,
+          }}
+        />
+      </li>
+    </Images>
+  )
+}
+
+const Images = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    p {
+      max-width: 20rem;
+      margin-bottom: 1.6rem;
+    }
+    > div {
+      height: 18rem;
+    }
+  }
+  h3 {
+    width: 12rem;
+    font-weight: 500;
+    text-align: center;
+  }
+  strong {
+    color: #000091;
+  }
+  img {
+    width: 6rem;
+    height: auto;
+    margin-bottom: 1rem;
+  }
+`
