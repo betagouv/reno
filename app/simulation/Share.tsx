28d8aa87198f884ee95874eed6175d9fd6de1a6d
Mael <laem@kont.me>
2024-01-17 11:01:45 +0100

    Section de partage plus complète et propre

--- app/simulation/Share.tsx
+++ app/simulation/Share.tsx
@@ -1,5 +1,9 @@
 import { Section } from '@/components/UI'
+import dynamic from '@/node_modules/next/dynamic'
 import Link from '@/node_modules/next/link'
+const ShareButton = dynamic(() => import('@/components/ShareButton'), {
+  ssr: false,
+})
 
 export default function LinkAPI({ searchParams }) {
   return (
@@ -7,11 +11,15 @@ export default function LinkAPI({ searchParams }) {
       <h2>Partage</h2>
 
       <p>
-        En partageant{' '}
-        <Link href={'?' + new URLSearchParams(searchParams)}>ce lien</Link> a
-        des amis, de la famille ou un conseiller, vous leur donnerez accès à vos
-        données et résultats de simulation.
+        En partageant ce lien à des amis, de la famille ou un conseiller, vous
+        leur donnerez accès à vos données et résultats de simulation.
       </p>
+      <ShareButton
+        {...{
+          text: 'Voici ma simulation Aides réno 2024',
+          title: 'Mes aides réno',
+        }}
+      />
     </Section>
   )
 }
