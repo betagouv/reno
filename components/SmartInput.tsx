c30cfc1e35868c3f7264dadf624f2c96831c824a
Mael <laem@kont.me>
2024-01-30 18:37:06 +0100

    Test de l'algo modifié de Maxime sur MAV

--- components/SmartInput.tsx
+++ components/SmartInput.tsx
@@ -3,17 +3,31 @@ import { useDebounce } from 'use-debounce'
 
 import { reduceAST } from 'publicodes'
 export default function Input({ onChange, value, rule, evaluation, engine }) {
-  console.log('smart', evaluation)
   const name = 'ménage . revenu . classe'
   const rulesUsingThisInput = [engine.getParsedRules()[name]]
   console.log(rulesUsingThisInput, engine.evaluate(name))
+  const comparaisonOperators = ['<', '<=', '>', '>=']
+  const convertValue = (value) => value
+  const searchedName = 'revenu'
   const accumulate = reduceAST(
-    (acc, next) => {
-      console.log('smart', next)
+    (acc, node) => {
+      console.log('smart kind', node.nodeKind, node.operationKind)
+      if (
+        node.nodeKind === 'operation' &&
+        comparaisonOperators.includes(node.operationKind)
+      ) {
+        if (node.explanation[0]?.dottedName === searchedName) {
+          return [...acc, convertValue(node.explanation[1])]
+        } else if (node.explanation[1]?.dottedName === searchedName) {
+          return [...acc, convertValue(node.explanation[0])]
+        }
+        return acc
+      }
+      return acc
     },
-    {},
+    [],
     engine.getRule(name),
   )
-  console.log('smart final', accumulate)
+  console.log('smart final', accumulate, engine.getRule(name))
   return 'salut'
 }
