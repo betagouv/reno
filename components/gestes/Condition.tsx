5f16e607bdb8341b302345b682d2c523aeccfd78
Mael <laem@kont.me>
2024-03-04 11:29:41 +0100

    Style pour l'avertissement

--- components/gestes/Condition.tsx
+++ components/gestes/Condition.tsx
@@ -1,6 +1,30 @@
+import Image from 'next/image'
 export default function Condition({}) {
   return (
-    <div css={``}>
+    <div
+      css={`
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        p {
+          width: 80%;
+          border-left: 3px solid var(--lighterColor);
+          padding-left: 0.6rem;
+        }
+        img {
+          width: 2rem;
+          height: auto;
+          margin-right: 1rem;
+        }
+        margin-top: 3vh;
+      `}
+    >
+      <Image
+        src={`/information.svg`}
+        width="10"
+        height="10"
+        alt="Icône d'information"
+      />
       <p>
         Pour bénéficier de la prime sur l'audit énergétique, les gestes
         d'isolation, de ventilation et de dépose de cuve à fioul, vous devrez
