e75e5bc534971cb7bcb4676e356dd80fcba48d7d
Mael <laem@kont.me>
2024-03-18 17:57:44 +0100

    MAJ texte confidentialité maintenant que l'on a Matomo

--- app/confidentialite/page.tsx
+++ app/confidentialite/page.tsx
@@ -12,19 +12,22 @@ export default function APIDoc() {
         <h2>Confidentialité</h2>
         <h3>Traceurs</h3>
         <p>
-          Nous n'utilisons pour l'instant aucun traceur. Quand vous faites une
-          simulation sur Mes Aides Réno, et que vous saisissez par exemple votre
-          catégorie de revenu, cette information est reçue par notre serveur
-          (car les données sont stockées dans la barre d'adresse de votre
-          navigateur), mais elles ne sont pas stockées.
+          Nous utilisons le traceur libre Matomo, via l'instance hébergée par
+          les services de l'État sur stats.beta.gouv.fr.
         </p>
         <p>
-          Par la suite, nous utiliserons probablement la solution Matomo
-          hébergée par l'État pour suivre l'audience du site et ainsi
-          l'améliorer, mais pour un suivi minimum qui ne nécessitera pas
-          l'acceptation préalable via un bandeau cookies.
+          Quand vous faites une simulation sur Mes Aides Réno, et que vous
+          saisissez par exemple votre catégorie de revenu, cette information est
+          reçue par notre serveur (car les données sont stockées dans la barre
+          d'adresse de votre navigateur), l'adresse est stockée dans notre outil
+          de suivi d'audience, mais aucun compte utilisateur nominatif n'est
+          créé et les données ne sont utilisées que pour améliorer les parcours
+          sur le site et la fiabilité du modèle de calcul.
         </p>
-
+        <p>
+          L'outil de suivi Matomo est configuré pour un suivi qui ne nécessite
+          pas de bannière de consentement RGPD (dite "cookies").
+        </p>
         <h3>Serveur</h3>
         <p>
           Notre serveur est géré par la société française{' '}
