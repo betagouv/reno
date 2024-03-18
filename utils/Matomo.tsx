7242522e44758709db5072978703d18ff8a98e0d
Mael <laem@kont.me>
2024-03-18 16:11:40 +0100

    Matomo suivra maintenant les changements de page SPA

--- utils/Matomo.tsx
+++ utils/Matomo.tsx
@@ -1,24 +1,30 @@
+'use client'
 import Script from 'next/script'
+import { init, push } from '@socialgouv/matomo-next'
+import { useEffect, useRef, useState } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 const MATOMO_URL = 'https://stats.beta.gouv.fr'
-const MATOMO_SITE_ID = 101
+const MATOMO_SITE_ID = '101'
 
 export default function Matomo() {
-  return (
-    <Script id="matomo" strategy="afterInteractive">
-      {`
-var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="${MATOMO_URL}/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '${MATOMO_SITE_ID}']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-`}
-    </Script>
-  )
+  const [initialised, setInitialised] = useState(false)
+  useEffect(() => {
+    if (MATOMO_URL && MATOMO_SITE_ID && initialised) {
+      init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
+    }
+    return () => {
+      setInitialised(true)
+    }
+  }, [initialised, setInitialised])
+
+  const searchParams = useSearchParams(),
+    pathname = usePathname()
+
+  const searchParamsString = searchParams.toString()
+  useEffect(() => {
+    if (!pathname) return
+    push(['trackPageView'])
+  }, [pathname, searchParamsString])
+  return null
 }
