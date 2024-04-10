417d1b8691e3a70cfa7f272f8e3b9d46a6d5930f
Mael <laem@kont.me>
2024-04-10 16:09:51 +0200

    Suppression des questions et du total de l'écran panier

--- components/GestesBasket.tsx
+++ components/GestesBasket.tsx
@@ -2,12 +2,10 @@ import MarSearch from '@/app/trouver-accompagnateur-renov/MarSearch'
 import Image from 'next/image'
 import Link from 'next/link'
 import { formatValue } from 'publicodes'
-import { BlocQuestionRéponse } from './BlocQuestionRéponse'
 import Geste from './Geste'
 import GesteQuestion from './GesteQuestion'
 import { gestesMosaicQuestions, isGestesMosaicQuestion } from './GestesMosaic'
 import { encodeDottedName } from './publicodes/situationUtils'
-import { Value } from './ScenariosSelector'
 import { Card, CTA, CTAWrapper } from './UI'
 
 export default function GestesBasket({
@@ -66,140 +64,10 @@ export default function GestesBasket({
               <Geste
                 {...{ dottedName: question[0], rules, engine, expanded: true }}
               />
-              <GesteQuestion
-                {...{
-                  dottedName: question[0],
-                  rules,
-                  nextQuestions,
-                  engine,
-                  situation,
-                  answeredQuestions,
-                  setSearchParams,
-                }}
-              />
             </Card>
           </li>
         ))}
       </ul>
-      <div>
-        <p
-          css={`
-            visibility: ${missingValues ? 'visible' : 'hidden'};
-            text-align: right;
-          `}
-        >
-          💡 Répondez aux questions ci-dessus pour obtenir une estimation de
-          l'aide totale.
-        </p>
-        <div
-          css={`
-            margin-top: 0.6rem;
-            position: sticky;
-            top: 2rem;
-            > div {
-              text-align: center;
-              border: 2px solid #7eb48f;
-              padding: 0.2rem 0.4rem;
-              background: #c4fad5;
-              width: 10rem;
-              margin: 0;
-              margin-left: auto;
-            }
-          `}
-        >
-          <div>Estimation totale {missingValues ? '...' : ` ~ ${total}`}</div>
-        </div>
-      </div>
-      <BlocQuestionRéponse>
-        <details>
-          <summary open={false}>Y a-t-il un montant maximum d'aides ?</summary>
-
-          <p>
-            Le montant cumulé de MaPrimeRénov’, des aides des fournisseurs
-            d’énergie et des aides versées par la Commission de régulation de
-            l’énergie en Outre-mer ne peut pas dépasser, pour votre ménage de
-            classe de revenu{' '}
-            <Value
-              {...{
-                engine,
-                situation: { ...situation },
-                dottedName: 'ménage . revenu . classe',
-                state: 'emphasize',
-              }}
-            />
-            , un maximum de{' '}
-            <Value
-              {...{
-                engine,
-                situation: { ...situation },
-                dottedName: "MPR . non accompagnée . pourcentage d'écrêtement",
-                state: 'emphasize',
-              }}
-            />{' '}
-            de la dépense éligible.
-          </p>
-          <p>
-            Le montant cumulé de MaPrimeRenov’ et de toutes les aides publiques
-            et privées perçues ne peut pas dépasser 100 % de la dépense éligible
-            après remise, ristourne ou rabais des entreprises.
-          </p>
-          <p>
-            Cette règle est appellée <em>écrêtement</em>.
-          </p>
-        </details>
-        <details>
-          <summary open={false}>
-            Avec quelles professionnels puis-je bénéficier de ces primes ?
-          </summary>
-          <p>
-            Les entreprises qui feront les travaux{' '}
-            <strong>
-              doivent être{' '}
-              <a
-                href="https://www.ecologie.gouv.fr/label-reconnu-garant-lenvironnement-rge"
-                target="_blank"
-              >
-                certifiées RGE
-              </a>
-            </strong>{' '}
-            pour que vous puissiez rentrer dans le parcours MaPrimeRénov' et
-            bénéficier des primes ci-dessus.
-          </p>
-        </details>
-        <details>
-          <summary open={false}>Les montants incluent-ils la pose ?</summary>
-          <p>
-            Oui. La dépense éligible correspond au coût du matériel, pose
-            comprise.
-          </p>
-        </details>
-        <details>
-          <summary open={false}>Peut-on cumuler les aides ?</summary>
-          <p>
-            Il est possible d’obtenir plusieurs fois MaPrimeRénov’ gestes pour
-            des travaux différents au sein d’un même logement (par exemple : des
-            travaux portant sur une autre surface du logement ou sur un autre
-            équipement), dans la limite de 20 000 € de travaux par logement sur
-            5 ans.
-          </p>
-          <p>
-            MaPrimeRénov’ gestes est cumulable avec les aides versées par les
-            fournisseurs d’énergie (CEE) ainsi que les aides des collectivités
-            locales et des caisses de retraite
-          </p>
-          <details>
-            <summary open={false}>Si vous êtes propriétaire bailleur</summary>
-
-            <p>
-              Les propriétaires bailleurs peuvent déposer des dossiers pour 3
-              logements diffé - rents maximum, dans la limite de 20 000€ de
-              travaux par logement sur 5 ans. Ils peuvent par ailleurs
-              bénéficier de MaPrimeRénov’ en tant que propriétaire occupant,
-              pour leur résidence principale ;
-            </p>
-          </details>
-        </details>
-      </BlocQuestionRéponse>
       <h2>C'est parti ?</h2>
       <p>
         Vous pouvez maintenant contacter un conseiller France Rénov'. Cela ne
