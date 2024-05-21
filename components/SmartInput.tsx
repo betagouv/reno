9abca9e9c5c74986caa00709a20976831538e2df
Mael <laem@kont.me>
2024-05-21 11:46:34 +0200

    Déplacement du très très modeste dans Angers

--- components/SmartInput.tsx
+++ components/SmartInput.tsx
@@ -1,9 +1,5 @@
-import { useEffect, useState } from 'react'
-import { useDebounce } from 'use-debounce'
+import { omit, sortBy } from './utils'
 
-import { reduceAST } from 'publicodes'
-import { formatValue } from '@/node_modules/publicodes/dist/index'
-import { omit } from './utils'
 export default function Input({ situation, onChange, value, rule, engine }) {
   /* First we went for Maxime's method on mesaidesvelo
    * consisting of doing a static analysis of candidate values,
@@ -22,29 +18,47 @@ export default function Input({ situation, onChange, value, rule, engine }) {
   const evaluation = engine
     .setSituation(omit(['ménage . revenu'], situation))
     .evaluate(targets[idf.nodeValue ? 0 : 1])
+
+  // We have multiple sources of thresholds to combine, since the introduction of aides-locales.yaml
+  // Another strategy would be to combine them in YAML before parsing : parsing created more complicated object structures
+  // I'm obviously not proud of this code. When we're getting serious, if more addition subventions include this kind of barème extension, we should code all this properly
+
   const activeBarème =
     evaluation.explanation.valeur.explanation.alors.explanation.find(
       (el) => el.condition.nodeValue,
     )
-  console.log('smart eval list', activeBarème.consequence.explanation)
-  const list = activeBarème.consequence.explanation
-    .map((el) => {
-      if (el.condition.isDefault) return false
-      console.log('smart eval condition', el.condition)
-      const conditionRightValue = engine.evaluate(
-        el.condition.explanation[1],
-      ).nodeValue
-      if (conditionRightValue != null) return conditionRightValue
 
-      throw new Error(
-        'Impossible de calculer les bornes de revenu intelligentes.',
+  const selectThresholds = (list) =>
+    list
+      .map((el) => {
+        if (el.condition.isDefault) return false
+        const conditionRightValue = engine.evaluate(
+          el.condition.explanation[1],
+        ).nodeValue
+        if (conditionRightValue != null) return conditionRightValue
+
+        throw new Error(
+          'Impossible de calculer les bornes de revenu intelligentes.',
+        )
+      })
+      .filter(Boolean)
+
+  const baseList = selectThresholds(activeBarème.consequence.explanation)
+
+  const additionalbarème =
+    !idf.nodeValue &&
+    engine
+      .evaluate(`métropole d'Angers . revenu très très modeste`)
+      .explanation.valeur.explanation.alors.explanation.find(
+        (branch) => branch.condition.nodeValue === true,
       )
-    })
-    .filter(Boolean)
 
-  console.log('smart eval list', list)
+  const additionalList = additionalbarème
+    ? [engine.evaluate(additionalbarème.consequence[1]).nodeValue]
+    : []
 
-  console.log('smart revenu', revenu)
+  const list = sortBy((el) => el)([...baseList, ...additionalList])
+
   const lastThreshold = list.slice(-1)[0]
   return [...list, Infinity].map((threshold, index) => {
     const valueToSet =
