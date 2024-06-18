1dc3d5f25e2e26e62c54289ec3a75a79874e5e33
Mael <laem@kont.me>
2024-06-18 14:21:47 +0200

    Page aide locale fonctionnelle

--- app/locales/[place]/Place.tsx
+++ app/locales/[place]/Place.tsx
@@ -33,12 +33,16 @@ const defaultSituationEntries = sortBy(([, score]) => score)(
       ],
   )
   .filter(Boolean)
+
 export default function LocalePlace({ place }) {
   const [situationEntries, setSituationEntries] = useState(
     defaultSituationEntries,
   )
 
   console.log('plop', missingVariables)
+  const placeRules = aidesEntries.filter(([dottedName, rule]) =>
+    dottedName.startsWith(place),
+  )
 
   const [engine, situation] = useMemo(() => {
     try {
@@ -82,8 +86,17 @@ export default function LocalePlace({ place }) {
       return [baseEngine, situation]
     }
   }, [situationEntries, baseEngine])
+
   return (
-    <Main>
+    <div css={``}>
+      <Section>
+        <h1>Aides locales {capitalise0(place)}</h1>
+        <p>
+          Découvrez ci-dessous les aides locales {capitalise0(place)}. Vous
+          pouvez changer la situation de votre ménage pour voir l'évolution du
+          montant et de l'éligibilité des aides.
+        </p>
+      </Section>
       <div
         css={`
           display: flex;
@@ -95,100 +108,104 @@ export default function LocalePlace({ place }) {
             position: fixed;
             top: 50%;
             transform: translateY(-50%);
-            left: 2rem;
+            left: 6rem;
           `}
         >
-          <h2>Situation</h2>
+          <h2>Votre situation</h2>
           <SituationEditor {...{ situationEntries, setSituationEntries }} />
         </div>
-        <Section>
-          <h1>Aides locales {capitalise0(place)}</h1>
-          <p>Blabla</p>
-        </Section>
-        <ul
+        <Section
           css={`
-            list-style-type: none;
+            margin-top: 2rem;
           `}
         >
-          {sortBy(
-            ([dottedName]) =>
-              dottedName == place || !dottedName.endsWith('montant'),
-          )(rules).map(([dottedName, rule]) => {
-            if (rule == null) return
+          <ul
+            css={`
+              list-style-type: none;
+            `}
+          >
+            {sortBy(
+              ([dottedName]) =>
+                dottedName == place || !dottedName.endsWith('montant'),
+            )(placeRules).map(([dottedName, rule]) => {
+              if (rule == null) return
 
-            const evaluation = engine.evaluate('aides locales . ' + dottedName)
-            const value = formatValue(evaluation)
+              const evaluation = engine.evaluate(
+                'aides locales . ' + dottedName,
+              )
+              const value = formatValue(evaluation)
 
-            const isMontant = dottedName.endsWith('montant')
+              const isMontant = dottedName.endsWith('montant')
 
-            return (
-              <li
-                key={dottedName}
-                css={`
-                  margin: 0.6rem 0;
-                `}
-              >
-                {dottedName !== place && (
-                  <div
-                    css={`
-                      display: flex;
-                      justify-content: space-between;
-                    `}
-                  >
-                    <span
-                      css={`
-                        display: flex;
-                        flex-direction: column;
-                      `}
-                    >
-                      <small>
-                        {parentName(dottedName).split(place + ' . ')[1]}
-                      </small>
-                      <h3
-                        css={`
-                          font-size: 100%;
-                          margin: 0;
-                          width: fit-content;
-                          ${isMontant && `background: yellow`}
-                        `}
-                      >
-                        {getRuleTitle(dottedName, aides)}
-                      </h3>
-                    </span>
-                    <span>{value}</span>
-                  </div>
-                )}
-                <div
+              return (
+                <li
+                  key={dottedName}
                   css={`
-                    > div {
-                      border: 1px solid #aaa;
-                      > ul {
-                        padding-left: 0.6rem;
-                        margin: 0.6rem 0;
-                      }
-                    }
+                    margin: 0.6rem 0;
                   `}
                 >
-                  {typeof rule === 'string' ? (
-                    <div>{rule}</div>
-                  ) : (
-                    <FriendlyObjectViewer
-                      {...{
-                        data: omit(['titre'], rule),
-                        options: {
-                          keyStyle: `
+                  {dottedName !== place && (
+                    <div
+                      css={`
+                        display: flex;
+                        justify-content: space-between;
+                      `}
+                    >
+                      <span
+                        css={`
+                          display: flex;
+                          flex-direction: column;
+                        `}
+                      >
+                        <small>
+                          {parentName(dottedName).split(place + ' . ')[1]}
+                        </small>
+                        <h3
+                          css={`
+                            font-size: 100%;
+                            margin: 0;
+                            width: fit-content;
+                            ${isMontant && `background: yellow`}
+                          `}
+                        >
+                          {getRuleTitle(dottedName, aides)}
+                        </h3>
+                      </span>
+                      <span>{value}</span>
+                    </div>
+                  )}
+                  <div
+                    css={`
+                      > div {
+                        border: 1px solid #aaa;
+                        > ul {
+                          padding-left: 0.6rem;
+                          margin: 0.6rem 0;
+                        }
+                      }
+                    `}
+                  >
+                    {typeof rule === 'string' ? (
+                      <div>{rule}</div>
+                    ) : (
+                      <FriendlyObjectViewer
+                        {...{
+                          data: omit(['titre'], rule),
+                          options: {
+                            keyStyle: `
 									color: #41438a
 									`,
-                        },
-                      }}
-                    />
-                  )}
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+                          },
+                        }}
+                      />
+                    )}
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </Section>
       </div>
-    </Main>
+    </div>
   )
 }
