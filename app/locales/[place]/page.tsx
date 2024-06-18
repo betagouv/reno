1578f96ccd6d26341530101e83aa862759ab4e2e
Mael <laem@kont.me>
2024-06-18 16:04:13 +0200

    Lien de retour aux aides locales

--- app/locales/[place]/page.tsx
+++ app/locales/[place]/page.tsx
@@ -1,11 +1,18 @@
-import { Main } from '@/components/UI'
+import { Main, Section } from '@/components/UI'
 import LocalePlace from './Place'
+import Link from 'next/link'
+import css from '@/components/css/convertToJs'
 
 export default function ({ params: { place: encodedPlace } }) {
   const place = decodeURIComponent(encodedPlace)
 
   return (
     <Main>
+      <Section>
+        <Link href="/locales" style={css``}>
+          ⬅️ Retour aux aides locales
+        </Link>
+      </Section>
       <LocalePlace place={place} />
     </Main>
   )
