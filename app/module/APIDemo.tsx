2ee7c3bbf444ab82095c429bf453dffb2781f947
Mael <laem@kont.me>
2024-07-08 15:02:16 +0200

    Début d'une page "module"

---
+++ app/module/APIDemo.tsx
@@ -0,0 +1,87 @@
+'use client'
+import { useMemo, useState } from 'react'
+import { parse, stringify } from 'yaml'
+import personas from '@/app/personas.yaml'
+import rules from '@/app/règles/rules'
+import Publicodes, { formatValue } from 'publicodes'
+import { useDebounce } from 'use-debounce'
+import styled from 'styled-components'
+import { Card } from '@/components/UI'
+
+const engine = new Publicodes(rules)
+export default function APIDemo() {
+  const [yaml, setYaml] = useState(stringify(personas[0].situation))
+  const [debouncedYaml] = useDebounce(yaml, 500)
+
+  const mpra = useMemo(() => {
+    console.log('memo')
+    try {
+      const json = parse(debouncedYaml)
+      const evaluation = engine
+        .setSituation(json)
+        .evaluate('MPR . accompagnée . montant')
+      return formatValue(evaluation, { precision: 0 })
+    } catch (e) {
+      console.log(e)
+      return e
+    }
+  }, [debouncedYaml])
+
+  return (
+    <div
+      css={`
+        margin: 2rem 0;
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: start;
+      `}
+    >
+      <h3>Les données d'entrée de votre plateforme d'annonce</h3>
+      <section>
+        <TextArea
+          value={yaml}
+          onChange={(e) => console.log('onchange') || setYaml(e.target.value)}
+        />
+        <h3>Le module de simulation que verra l'usager</h3>
+        <ul>
+          <li>Pas pour résidence secondaire</li>
+          <li>
+            <input placeholder="Votre revenu" />
+          </li>
+          <li>
+            <input placeholder="Votre revenu" />
+          </li>
+          <li>Ile de France ?</li>
+        </ul>
+        <Card>
+          <EvaluationValue>
+            <small>MaPrimeRénov' accompagnée</small>{' '}
+            <div>
+              {typeof mpra === 'string' ? mpra : <p>{mpra.toString()}</p>}
+            </div>
+          </EvaluationValue>
+        </Card>
+      </section>
+    </div>
+  )
+}
+
+export const TextArea = styled.textarea`
+  padding: 0.6rem;
+  font-size: 110%;
+  width: 25rem;
+  height: 10rem;
+  border: 2px solid var(--color);
+  margin-right: 2rem;
+`
+
+export const EvaluationValue = styled.div`
+  background: var(--color);
+  color: white;
+  padding: 1rem 2rem;
+  text-align: center;
+  small {
+    margin-bottom: 0.4rem;
+  }
+`
---
+++ app/module/page.tsx
@@ -0,0 +1,91 @@
+import { Main, Section } from '@/components/UI'
+import css from '@/components/css/convertToJs'
+import Link from '@/node_modules/next/link'
+import { Metadata } from 'next/types'
+import APIDemo from './APIDemo'
+export const metadata: Metadata = {
+  title: 'API - Mes aides réno',
+  description:
+    "Découvrez la documentation de l'API de calcul des aides à la rénovation",
+}
+
+export default function APIDoc() {
+  return (
+    <Main>
+      <Section>
+        <h2>Module de calcul d'aide pour un achat de passoire thermique</h2>
+
+        <p>Vous pouvez utiliser ce calculateur via notre API. </p>
+        <p>
+          C'est une API <a href="https://publi.codes">Publicodes</a>. Nous vous
+          conseillons de faire un petit tour (10&nbsp;minutes) sur la
+          documentation de Publicodes pour mieux comprendre ses fondamentaux.
+        </p>
+        <h3>Démonstration</h3>
+        <APIDemo />
+        <h3>Que permet-elle ?</h3>
+        <p>
+          Elle permet de calculer les deux parcours Ma Prime Rénov' 2024,
+          accompagné et non accompagné, à partir de la situation d'un
+          utilisateur. La situation comprend le revenu fiscal du ménage, les
+          sauts de DPE envisagés, mais aussi le projet d'isolation par geste, et
+          quelques autres données.
+        </p>
+        <p>
+          L'API n'est pour l'instant disponible qu'en méthode <em>GET</em> :
+          tous les paramètres de la simulation sont à sérialiser dans l'unique
+          URL de simulation.
+        </p>
+        <h3>Que renvoie-t-elle ?</h3>
+        <p>
+          L'API vous renvoie, pour chacun des deux dispositifs de Ma Prime
+          Rénov' : le résultat numérique ou 'Non applicable', la liste des
+          questions auxquelles l'utilisateur doit encore répondre (c'est une API
+          conversationnelle), ainsi que l'objet complet de simulation
+          Publicodes.
+        </p>
+        <h3>Spécification</h3>
+        <p>
+          Pour découvrir l'API, le plus simple est de faire votre simulation sur
+          la page d'accueil, ou de cliquer directement sur un persona pour
+          charger une des simulations pré-remplies, puis de préfixer l'URL de
+          simulation par `/api?PARAMÈTRES`.
+        </p>
+        <p>
+          Publicodes offre nativement une documentation Web qui vous permet
+          d'explorer les calculs de façon granulaire. Pour la découvrir, suivez
+          les liens "Inspection" de la{' '}
+          <a href="/personas#tests">section "Tests" de la page personas</a>.
+        </p>
+        <h4>Mode de simulation</h4>
+        <p>
+          Le modèle de calcul offre deux modes de simulation : le mode "max" qui
+          maximise les aides, et le mode "moyen". Ce mode ne vous importe que si
+          vous voulez afficher un résultat à l'utilisateur avant qu'il finisse
+          de répondre à toutes les questions. Les réponses manquantes (appellées{' '}
+          <em>missing variables</em> dans Publicodes) seront remplacées par au
+          choix, des valeurs maximales ou des valeurs moyennes estimées.
+        </p>
+        <h3>Le code</h3>
+        <p>
+          Tout le code du calculateur (site en NextJS), l'API (Route handler
+          NextJS) ainsi que les règles de calcul complètes sont disponibles sur{' '}
+          <Link href="https://github.com/betagouv/reno">Github</Link>. Les
+          règles sont aussi accessibles en JSON{' '}
+          <Link href="/api/rules">cette addresse</Link>.
+        </p>
+        <p>
+          Plutôt que de dépendre d'une API tierce, si vous avez confiance dans
+          votre capacité à mettre des services en ligne, le mieux reste
+          d'intégrer le moteur de calcul publicodes chez vous. C'est ce qui rend
+          la démonstration plus haut si fluide, les calculs sont faits{' '}
+          <em>dans votre navigateur, sans appel réseau</em>. Si vous êtes dans
+          un environnement Javascript, il suffit de quelques lignes de code.
+          Sinon, un simple projet Javascript avec ExpressJs permet de faire
+          tourner une API en 30 minutes sur vos serveurs. Nous publierons un
+          paquet NPM tout prêt.
+        </p>
+      </Section>
+    </Main>
+  )
+}
