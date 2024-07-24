cb0a52329a274b8da4e077e4867c24d8dc08e707
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-07-24 12:15:01 +0200

    Ajout de cas d'utilisation sur la home

--- app/LandingUI.tsx
+++ app/LandingUI.tsx
@@ -103,6 +103,54 @@ export const HomeList = styled.ol`
   }
 `
 
+export const HomeTestimonies = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: top;
+  list-style-type: none;
+  padding-left: 0;
+  text-align: left;
+  .fr-quote {
+    width: 50%;
+    padding: 1rem;
+    h3 {
+      font-size: 1.2rem;
+      margin: 0 0 1rem 1rem;
+    }
+    &:before {
+      color: #6a6af4;
+      content: "";
+      display: block;
+      margin-bottom: .5rem;
+      background-color: currentColor;
+      display: inline-block;
+      flex: 0 0 auto;
+      height: 2rem;
+      -webkit-mask-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+PHBhdGggZD0iTTE0IDNhOCA4IDAgMSAxIDAgMTZ2My41Yy01LTItMTItNS0xMi0xMS41YTggOCAwIDAgMSA4LThoNFptMCAyaC00YTYgNiAwIDAgMC02IDZjMCAzLjYxIDIuNDYyIDUuOTY2IDggOC40OFYxN2gyYTYgNiAwIDEgMCAwLTEyWm0tMiAyLjUtMi41MDYgMy43NUwxMiAxNUg5LjI5NUw2Ljc1IDExLjI1IDkuMjk1IDcuNUgxMlptNC41IDAtMi41MDYgMy43NUwxNi41IDE1aC0yLjcwNWwtMi41NDUtMy43NSAyLjU0NS0zLjc1SDE2LjVaIi8+PC9zdmc+);
+      mask-image: url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCI+PHBhdGggZD0iTTE0IDNhOCA4IDAgMSAxIDAgMTZ2My41Yy01LTItMTItNS0xMi0xMS41YTggOCAwIDAgMSA4LThoNFptMCAyaC00YTYgNiAwIDAgMC02IDZjMCAzLjYxIDIuNDYyIDUuOTY2IDggOC40OFYxN2gyYTYgNiAwIDEgMCAwLTEyWm0tMiAyLjUtMi41MDYgMy43NUwxMiAxNUg5LjI5NUw2Ljc1IDExLjI1IDkuMjk1IDcuNUgxMlptNC41IDAtMi41MDYgMy43NUwxNi41IDE1aC0yLjcwNWwtMi41NDUtMy43NSAyLjU0NS0zLjc1SDE2LjVaIi8+PC9zdmc+);
+      -webkit-mask-size: 100% 100%;
+      mask-size: 100% 100%;
+      vertical-align: calc(.375em - 1rem);
+      width: 2rem;
+    }
+    .see-more {
+      max-height: 0;
+      overflow: hidden;
+      transition: max-height 0.5s ease;
+    }
+    .see-more.expanded {
+      max-height: 300px;
+    }
+    .see-more-click {
+        cursor: pointer;
+        display: block;
+        text-align: right;
+        text-decoration: underline;
+    }
+  }
+`
+
 export const BlueEm = styled.em`
   color: var(--lightColor);
   font-style: normal;
