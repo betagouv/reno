32cccc0d82bfa78fe7ef641f023f1d6ff0b7c584
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-08-01 17:57:45 +0200

    Petite amélioration graphique et de code sur le simulateur MPR

--- components/mprg/BlocAideMPR.tsx
+++ components/mprg/BlocAideMPR.tsx
@@ -11,32 +11,27 @@ export const BlocAideMPR = ({ infoMPR, rules, engine, situation, setSearchParams
   const rawSearchParams = useSearchParams(),
   situationSearchParams = Object.fromEntries(rawSearchParams.entries())
 
-  const validatedQuestion = getAnsweredQuestions(situationSearchParams, rules)
-
   // On affiche les questions répondues, mais pas celles validées (sinon elles s'affichent lors du parcours par geste)
   const questionsAnswered = Object.keys(situation)
-                                  .filter(value => infoMPR.questions.includes(value))
-                                  .filter(value => !validatedQuestion.includes(value))
+                                  .filter(q => infoMPR.questions.includes(q) && !getAnsweredQuestions(situationSearchParams, rules).includes(q))
 
   const currentQuestion = infoMPR.questions[questionsAnswered.length];
-  
-  const isExactTotal = infoMPR.questions.filter((q) => rules[q].question)
-                                        .every(e => Object.keys(situation).includes(e))
+  const isExactTotal =  Array.isArray(infoMPR.questions) && infoMPR.questions.every(question => question in situation)
   const isEligible = infoMPR.montant !== "Non applicable"
   
   return (
         <BlocAide>
           <div className="aide-header">
             <Image src={mprImage} alt="logo ma prime renov" width="100" />
-            {displayPrime == "top" && (
               <div>
-                <PrimeStyle>
-                  {'Prime de '}
-                  <strong>{infoMPR.montant}</strong>
-                </PrimeStyle>
-                <h3>MaPrimeRénov'</h3>
+                {displayPrime === "top" && (
+                  <PrimeStyle>
+                    {'Prime de '}
+                    <strong>{infoMPR.montant}</strong>
+                  </PrimeStyle>
+                )}
+                <h3>{displayPrime !== "top" ? "Calculateur d'aide" : ""} MaPrimeRénov'</h3>
               </div>
-            )}
           </div>
           <div className="aide-details">
             {questionsAnswered.map((question, index) => (
