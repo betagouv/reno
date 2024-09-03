cdaa50a686aa3095374044524f1244ce6ad9ddf7
Mael <laem@kont.me>
2024-09-03 09:58:50 +0200

    Ajout d'un flux RSS du blog

---
+++ lib/rss.ts
@@ -0,0 +1,47 @@
+import { writeFileSync } from 'fs'
+import RSS from 'rss'
+import { articles } from '@/app/blog/[slug]/page.tsx'
+
+// Credits :
+// https://notebook.lachlanjc.com/2022-08-18_set_up_rss_with_contentlayer_and_mdx
+//
+const domain = 'https://mesaidesreno.beta.gouv.fr'
+export async function generateFeed() {
+  const feed = new RSS({
+    title: 'Mes Aides Réno - le blog',
+    site_url: domain,
+    feed_url: domain + '/feed.xml',
+    image_url: domain + '/jaquette.png',
+    language: 'fr_FR',
+  })
+
+  const capitalLetter = /^[A-Z]/
+
+  await Promise.all(
+    articles.map(async (post) => {
+      feed.item({
+        title: post.titre,
+        url: domain + '/blog/' + post.slug,
+        guid: post.slug,
+        date: post.date,
+        /*
+        description: await remark()
+          .use(strip)
+          .process(
+            post.body.raw
+              .split('\n\n')
+              .filter((text) => text.match(capitalLetter))?.[0],
+          )
+          .then((file) => String(file).trim()),
+		  */
+        description: post.description,
+      })
+    }),
+  )
+
+  const body = feed.xml({ indent: true })
+  writeFileSync('./.next/static/feed.xml', body)
+  console.log('🗞️ RSS feed written in .next/static/feed.xml')
+}
+
+generateFeed()
