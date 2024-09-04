ba50574c6c4fec601196f07be289a84a18a13ee0
Mael <laem@kont.me>
2024-09-04 18:04:42 +0200

    Essai de filtrage des ECFR par département

--- app/trouver-accompagnateur-renov/Entreprises.tsx
+++ app/trouver-accompagnateur-renov/Entreprises.tsx
@@ -1,6 +1,9 @@
 import Entreprise from './Entreprise'
 
+const département = (codeCommune) => ('' + codeCommune).slice(0, 2)
 export default function Entreprises({ data, codeInsee }) {
+  console.log('cyan data', data)
+  if (!codeInsee) return null
   // pas possible, on filtre data en amont, TODO gérer les erreurs de requête
   if (!data) return <p>Aucun conseiller trouvé :/</p>
   const sameCity = data.filter((el) => el.code_insee_commune === codeInsee)
@@ -27,6 +30,16 @@ export default function Entreprises({ data, codeInsee }) {
       </ol>
     )
   }
+  const givenDépartement = département(codeInsee)
+
+  const closestSameDépartement = data.find(
+    (el) => département(el.code_insee_commune) === givenDépartement,
+  )
+  if (
+    département(data[0].code_insee_commune) !== givenDépartement &&
+    closestSameDépartement.distance < 3 * data[0].distance
+  )
+    return <Entreprise data={closestSameDépartement} />
 
   return <Entreprise data={data[0]} />
 }
