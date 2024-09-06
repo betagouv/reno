53a901b7f08a324ffab3ead28ae4f4d2230cb8d9
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-09-06 11:02:04 +0200

    Ajout d'actual immo

--- app/HomepageTalkAboutUs.tsx
+++ app/HomepageTalkAboutUs.tsx
@@ -4,6 +4,7 @@ import useIsInIframe from "@/components/useIsInIframe"
 import logoBonPote from '@/public/logo-partenaire/logo-bon-pote.jpg'
 import logoFranceInfo from '@/public/logo-partenaire/logo-france-info.jpg'
 import logoTf1Info from '@/public/logo-partenaire/logo-tf1-info.svg'
+import logoActualImmo from '@/public/logo-partenaire/logo-actual-immo.png'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -18,23 +19,31 @@ export default function HomepageTalkAboutUs() {
                 <Link
                     href="https://www.francetvinfo.fr/economie/immobilier/logements-bouilloires-ces-obstacles-qui-freinent-l-adaptation-aux-fortes-chaleurs_6737814.html"
                     target="_blank"
-                    css={`width: 33%;min-width: 200px;`}
+                    css={`width: 25%;min-width: 200px;`}
                 >
                     <Image src={logoFranceInfo} alt="Logo France Info" />
                 </Link>
                 <Link
                     href="https://bonpote.com/connaitre-en-quelques-clics-les-aides-de-letat-pour-renover-son-logement/"
                     target="_blank"
-                    css={`width: 33%;min-width: 200px;`}
+                    css={`width: 25%;min-width: 200px;`}
                 >
                     <Image src={logoBonPote} alt="Logo Bon Pote" />
                 </Link>
                 <Link
                     href="https://www.tf1info.fr/immobilier/bouilloires-thermiques-comment-adapter-son-logement-aux-vagues-de-chaleur-2315763.html"
                     target="_blank"
-                    css={`width: 33%;min-width: 200px;`}
+                    css={`width: 25%;min-width: 200px;`}
                 >
                     <Image src={logoTf1Info} alt="Logo TF1 Info" 
+                        css={`width: 100%;`}/>
+                </Link>
+                <Link
+                    href="https://www.actual-immo.fr/investissement-passoires-energetiques/"
+                    target="_blank"
+                    css={`width: 25%;min-width: 200px;`}
+                >
+                    <Image src={logoActualImmo} alt="Logo Actual Immo" 
                         css={`width: 100%;`}/>
                 </Link>
             </div>
