6fda2ee83d065666c15b0675675ed94087994b1d
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-09-23 10:55:18 +0200

    le header passe en footer avec un CTA pour arriver sur le site principal

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,226 +1,231 @@
 'use client'
 
 import { FooterWrapper, ExternalLink, InternalLink } from './UI'
-import useIsInIframe from '@/components/useIsInIframe'
+import useIsInIframe, { useIsCompact } from '@/components/useIsInIframe'
 import { allArticles } from '@/.contentlayer/generated'
 import { sortBy } from './utils'
+import FooterCompact from './FooterCompact'
 
 export default function Footer() {
   const isInIframe = useIsInIframe()
+  const isCompact = useIsCompact()
   const articles = [...sortBy((article) => article.date)(allArticles).reverse()]
 
   return (
-    !isInIframe && (
-      <>
-        <FooterWrapper className="fr-footer" role="contentinfo">
-          <div>
-            <div className="fr-footer__top">
-              <div className="footer-col">
-                <h3 className="fr-footer__top-cat">
-                  <InternalLink className="fr-footer__top-link" href={'/blog'}>
-                    Blog
-                  </InternalLink>
-                </h3>
-                <ul className="fr-footer__top-list">
-                  {articles
-                    .filter(
-                      ({ tags, brouillon }) =>
-                        !tags?.includes('notes de version') && !brouillon,
-                    )
-                    .map(({ url, titre }) => (
-                      <li key={url}>
-                        <InternalLink
-                          className="fr-footer__top-link"
-                          href={url}
-                        >
-                          {titre}
-                        </InternalLink>
-                      </li>
-                    ))}
-                </ul>
+    <>
+      { !isInIframe && (
+        <>
+          <FooterWrapper className="fr-footer" role="contentinfo">
+            <div>
+              <div className="fr-footer__top">
+                <div className="footer-col">
+                  <h3 className="fr-footer__top-cat">
+                    <InternalLink className="fr-footer__top-link" href={'/blog'}>
+                      Blog
+                    </InternalLink>
+                  </h3>
+                  <ul className="fr-footer__top-list">
+                    {articles
+                      .filter(
+                        ({ tags, brouillon }) =>
+                          !tags?.includes('notes de version') && !brouillon,
+                      )
+                      .map(({ url, titre }) => (
+                        <li key={url}>
+                          <InternalLink
+                            className="fr-footer__top-link"
+                            href={url}
+                          >
+                            {titre}
+                          </InternalLink>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+                <div className="footer-col">
+                  <h3 className="fr-footer__top-cat">
+                    <InternalLink href="/aides">Les aides</InternalLink>
+                  </h3>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/aides/ma-prime-renov"
+                      >
+                        MaPrimeRénov
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/aides/coup-de-pouce"
+                      >
+                        Les Coups de pouce
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/aides/cee"
+                      >
+                        Certificats d'économie d'énergie (CEE)
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="locales"
+                      >
+                        Les aides des collectivités locales
+                      </InternalLink>
+                    </li>
+                  </ul>
+                  {/* 
+                    <li><InternalLink className="fr-footer__top-link" href="#">L'éco-prêt à taux zéro</InternalLink></li>
+                    <li><InternalLink className="fr-footer__top-link" href="#">L'exonération de taxe foncière</InternalLink></li>
+                    <li><InternalLink className="fr-footer__top-link" href="#">Le dispositif Denormandie</InternalLink></li>
+                  */}
+                </div>
+                <div className="footer-col">
+                  <h3 className="fr-footer__top-cat">Nos partenaires</h3>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <ExternalLink
+                        target="_blank"
+                        className="fr-footer__top-link"
+                        title="France Rénov'"
+                        href="https://france-renov.gouv.fr"
+                      >
+                        France Rénov'
+                      </ExternalLink>
+                    </li>
+                    <li>
+                      <ExternalLink
+                        target="_blank"
+                        className="fr-footer__top-link"
+                        title="Dossier Facile"
+                        href="https://www.dossierfacile.logement.gouv.fr"
+                      >
+                        Dossier Facile
+                      </ExternalLink>
+                    </li>
+                    <li>
+                      <ExternalLink
+                        target="_blank"
+                        className="fr-footer__top-link"
+                        title="France Chaleur Urbaine"
+                        href="https://france-chaleur-urbaine.beta.gouv.fr"
+                      >
+                        France Chaleur Urbaine
+                      </ExternalLink>
+                    </li>
+                    <li>
+                      <ExternalLink
+                        target="_blank"
+                        className="fr-footer__top-link"
+                        title="Pacoupa"
+                        href="https://pacoupa.ademe.fr"
+                      >
+                        Pacoupa
+                      </ExternalLink>
+                    </li>
+                    <li>
+                      <ExternalLink
+                        target="_blank"
+                        className="fr-footer__top-link"
+                        title="Zéro Logement Vacant"
+                        href="https://zerologementvacant.beta.gouv.fr"
+                      >
+                        Zéro Logement Vacant
+                      </ExternalLink>
+                    </li>
+                  </ul>
+                </div>
+                <div className="footer-col">
+                  <h3 className="fr-footer__top-cat">Liens utiles</h3>
+                  <ul className="fr-footer__top-list">
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/a-propos"
+                      >
+                        À propos
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink className="fr-footer__top-link" href="/faq">
+                        Questions fréquentes 
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink className="fr-footer__top-link" href="/contact">
+                        Contact 
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/api-doc"
+                      >
+                        API
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/integration"
+                      >
+                        Intégrer notre calculateur
+                      </InternalLink>
+                    </li>
+                    <li>
+                      <InternalLink
+                        className="fr-footer__top-link"
+                        href="/personas"
+                      >
+                        Personas
+                      </InternalLink>
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div className="footer-col">
-                <h3 className="fr-footer__top-cat">
-                  <InternalLink href="/aides">Les aides</InternalLink>
-                </h3>
-                <ul className="fr-footer__top-list">
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/aides/ma-prime-renov"
-                    >
-                      MaPrimeRénov
+              <div className="fr-footer__bottom">
+                <ul className="fr-footer_bottom-list">
+                  <li className="fr-footer__bottom-item">
+                    <InternalLink href="/accessibilite">
+                      Accessibilité : non conforme
                     </InternalLink>
                   </li>
-                  <li>
+                  <li className="fr-footer__bottom-item">
                     <InternalLink
                       className="fr-footer__top-link"
-                      href="/aides/coup-de-pouce"
+                      href="/confidentialite"
                     >
-                      Les Coups de pouce
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/aides/cee"
-                    >
-                      Certificats d'économie d'énergie (CEE)
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="locales"
-                    >
-                      Les aides des collectivités locales
+                      Confidentialité
                     </InternalLink>
                   </li>
                 </ul>
-                {/* 
-                  <li><InternalLink className="fr-footer__top-link" href="#">L'éco-prêt à taux zéro</InternalLink></li>
-                  <li><InternalLink className="fr-footer__top-link" href="#">L'exonération de taxe foncière</InternalLink></li>
-                  <li><InternalLink className="fr-footer__top-link" href="#">Le dispositif Denormandie</InternalLink></li>
-                */}
-              </div>
-              <div className="footer-col">
-                <h3 className="fr-footer__top-cat">Nos partenaires</h3>
-                <ul className="fr-footer__top-list">
-                  <li>
+                <div className="fr-footer__bottom-copy">
+                  <p>
+                    Sauf mention contraire, tous les contenus de ce site sont sous{' '}
                     <ExternalLink
+                      href="https://github.com/betagouv/reno/blob/master/LICENSE"
                       target="_blank"
-                      className="fr-footer__top-link"
-                      title="France Rénov'"
-                      href="https://france-renov.gouv.fr"
+                      title="licence MIT - nouvelle fenêtre"
+                      css={`
+                        text-decoration: underline;
+                      `}
                     >
-                      France Rénov'
+                      licence MIT
                     </ExternalLink>
-                  </li>
-                  <li>
-                    <ExternalLink
-                      target="_blank"
-                      className="fr-footer__top-link"
-                      title="Dossier Facile"
-                      href="https://www.dossierfacile.logement.gouv.fr"
-                    >
-                      Dossier Facile
-                    </ExternalLink>
-                  </li>
-                  <li>
-                    <ExternalLink
-                      target="_blank"
-                      className="fr-footer__top-link"
-                      title="France Chaleur Urbaine"
-                      href="https://france-chaleur-urbaine.beta.gouv.fr"
-                    >
-                      France Chaleur Urbaine
-                    </ExternalLink>
-                  </li>
-                  <li>
-                    <ExternalLink
-                      target="_blank"
-                      className="fr-footer__top-link"
-                      title="Pacoupa"
-                      href="https://pacoupa.ademe.fr"
-                    >
-                      Pacoupa
-                    </ExternalLink>
-                  </li>
-                  <li>
-                    <ExternalLink
-                      target="_blank"
-                      className="fr-footer__top-link"
-                      title="Zéro Logement Vacant"
-                      href="https://zerologementvacant.beta.gouv.fr"
-                    >
-                      Zéro Logement Vacant
-                    </ExternalLink>
-                  </li>
-                </ul>
-              </div>
-              <div className="footer-col">
-                <h3 className="fr-footer__top-cat">Liens utiles</h3>
-                <ul className="fr-footer__top-list">
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/a-propos"
-                    >
-                      À propos
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink className="fr-footer__top-link" href="/faq">
-                      Questions fréquentes 
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink className="fr-footer__top-link" href="/contact">
-                      Contact 
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/api-doc"
-                    >
-                      API
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/integration"
-                    >
-                      Intégrer notre calculateur
-                    </InternalLink>
-                  </li>
-                  <li>
-                    <InternalLink
-                      className="fr-footer__top-link"
-                      href="/personas"
-                    >
-                      Personas
-                    </InternalLink>
-                  </li>
-                </ul>
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="fr-footer__bottom">
-              <ul className="fr-footer_bottom-list">
-                <li className="fr-footer__bottom-item">
-                  <InternalLink href="/accessibilite">
-                    Accessibilité : non conforme
-                  </InternalLink>
-                </li>
-                <li className="fr-footer__bottom-item">
-                  <InternalLink
-                    className="fr-footer__top-link"
-                    href="/confidentialite"
-                  >
-                    Confidentialité
-                  </InternalLink>
-                </li>
-              </ul>
-              <div className="fr-footer__bottom-copy">
-                <p>
-                  Sauf mention contraire, tous les contenus de ce site sont sous{' '}
-                  <ExternalLink
-                    href="https://github.com/betagouv/reno/blob/master/LICENSE"
-                    target="_blank"
-                    title="licence MIT - nouvelle fenêtre"
-                    css={`
-                      text-decoration: underline;
-                    `}
-                  >
-                    licence MIT
-                  </ExternalLink>
-                </p>
-              </div>
-            </div>
-          </div>
-        </FooterWrapper>
-      </>
-    )
+          </FooterWrapper>
+        </>
+      )}
+      { isCompact && (<FooterCompact />)}  
+    </>
   )
 }
