5f42ef446b0fd9b1d1ceca4b4784ee4a38d9c6c6
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-09-23 10:54:23 +0200

    Déplacer le bloc Plus d'information dans la Card de la question et l'afficher seulement au clic

--- components/QuestionDescription.tsx
+++ components/QuestionDescription.tsx
@@ -2,7 +2,10 @@ import informationIcon from '@/public/information.svg'
 import Image from 'next/image'
 import css from './css/convertToJs'
 import { Card } from './UI'
+import { useIsCompact } from './useIsInIframe'
 export default function QuestionDescription({ currentQuestion, rule }) {
+  
+  const isCompact = useIsCompact()
   return (
     currentQuestion &&
     rule.description && (
@@ -12,41 +15,88 @@ export default function QuestionDescription({ currentQuestion, rule }) {
           align-items: center;
         `}
       >
-        <Card
-          css={`
-            width: 100%;
-          `}
-        >
-          <span
-            css={`
-              display: flex;
-              align-items: center;
-              margin-bottom: 0.8rem;
-              color: #2a82dd;
-              font-weight: 500;
-            `}
-          >
-            <Image
-              src={informationIcon}
-              width="25"
-              style={css`
-                margin-right: 0.4rem;
+        { isCompact ? 
+            <details>
+              <summary 
+                css={`
+                  outline: none;
+                  list-style: none;
+                  text-align: justify;
+                  &::-webkit-details-marker {
+                    display: none;
+                  }
+                  &::marker {
+                    display: none;
+                  }`}
+              >
+                <span
+                  css={`
+                    display: flex;
+                    align-items: center;
+                    margin-bottom: 0.8rem;
+                    color: #2a82dd;
+                    font-weight: 500;
+                  `}
+                >
+                  <Image
+                    src={informationIcon}
+                    width="25"
+                    style={css`
+                      margin-right: 0.4rem;
+                    `}
+                  />{' '}
+                  <small>Plus d'informations</small>
+                </span>
+              </summary>
+              <div
+                dangerouslySetInnerHTML={{ __html: rule.descriptionHtml }}
+                css={`
+                  blockquote {
+                    margin-top: 0.8rem;
+                    border-left: 4px solid var(--lighterColor);
+                    padding: 0 0.6rem;
+                    color: #333;
+                  }
+                `}
+              ></div>
+            </details>
+          :
+            <Card
+              css={`
+                width: 100%;
               `}
-            />{' '}
-            <small>Plus d'informations</small>
-          </span>
-          <div
-            dangerouslySetInnerHTML={{ __html: rule.descriptionHtml }}
-            css={`
-              blockquote {
-                margin-top: 0.8rem;
-                border-left: 4px solid var(--lighterColor);
-                padding: 0 0.6rem;
-                color: #333;
-              }
-            `}
-          ></div>
-        </Card>
+            >
+              <span
+                css={`
+                  display: flex;
+                  align-items: center;
+                  margin-bottom: 0.8rem;
+                  color: #2a82dd;
+                  font-weight: 500;
+                `}
+              >
+                <Image
+                  src={informationIcon}
+                  width="25"
+                  style={css`
+                    margin-right: 0.4rem;
+                  `}
+                />{' '}
+                <small>Plus d'informations</small>
+              </span>
+              <div
+                dangerouslySetInnerHTML={{ __html: rule.descriptionHtml }}
+                css={`
+                  blockquote {
+                    margin-top: 0.8rem;
+                    border-left: 4px solid var(--lighterColor);
+                    padding: 0 0.6rem;
+                    color: #333;
+                  }
+                `}
+              ></div>
+            </Card>
+        }
       </div>
     )
   )
