80c99a743b79d67b8df1315693f0336d5c8f585b
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-10-02 12:05:20 +0200

    Amélioration de l'autocomplete des villes, attention necessite changement pour etre compatible avec logement . commune

--- components/AddressSearch.tsx
+++ components/AddressSearch.tsx
@@ -2,6 +2,7 @@ import { Loader } from '@/app/trouver-accompagnateur-renov/UI'
 import { useEffect, useState } from 'react'
 import css from './css/convertToJs'
 import { useDebounce } from 'use-debounce'
+import styled from 'styled-components'
 
 function onlyNumbers(str) {
   return /^\d+/.test(str)
@@ -13,7 +14,7 @@ export default function AddressSearch({ setChoice, situation }) {
   const [input] = useDebounce(immediateInput, 300)
 
   const [results, setResults] = useState(null)
-  const [clicked, setClicked] = useState(null)
+  const [clicked, setClicked] = useState(false)
   const validInput = input && input.length >= 3
 
   useEffect(() => {
@@ -22,7 +23,10 @@ export default function AddressSearch({ setChoice, situation }) {
         `https://geo.api.gouv.fr/communes?code=${situation['ménage . commune'].replace(/"/g, '')}`,
       )
         .then((response) => response.json())
-        .then((json) => setInput(json[0].nom + ' ' + json[0].codeDepartement))
+        .then((json) => {
+          setInput(json[0].nom + ' ' + json[0].codeDepartement)
+          setClicked(true)
+        })
     }
   }, [situation, setInput])
 
@@ -61,11 +65,18 @@ export default function AddressSearch({ setChoice, situation }) {
       `}
     >
       <input
+        css={`
+          margin: 0;
+          ${clicked && input && `border-bottom: 2px solid var(--validColor) !important;`}
+        `}
         type="text"
         autoFocus={true}
         value={immediateInput}
         placeholder={'commune ou code postal'}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={(e) => {
+          setClicked(false)
+          setInput(e.target.value)
+        }}
       />
       {validInput && !results && (
         <div
@@ -79,36 +90,52 @@ export default function AddressSearch({ setChoice, situation }) {
           Chargement...
         </div>
       )}
-      {results && (
-        <ul
-          style={css`
-            margin-top: 0.6rem;
-            width: 20rem;
-            max-width: 90vw;
-            list-style-type: none;
-          `}
-        >
+      {results && !clicked && (
+        <CityList>
+          <li css={`color: #929292;`}>Sélectionnez une ville</li>
           {results.map((result) => (
             <li
+              className={situation['ménage . commune'] && situation['ménage . commune'].replace(/"/g, '') == result.code ? "selected" : ""}
               key={result.code}
               onClick={() => {
-                setClicked(result)
                 setChoice(result)
                 setInput(result.nom + ' ' + result.codeDepartement)
+                setClicked(true)
               }}
-              css={`
-                cursor: pointer;
-                text-align: right;
-                ${clicked &&
-                clicked.code === result.code &&
-                `background: var(--lighterColor)`}
-              `}
             >
-              {result.nom} <small>{result.codeDepartement}</small>
+              {result.nom} <small>{result?.codesPostaux[0]}</small>
             </li>
           ))}
-        </ul>
+        </CityList>
       )}
+      {clicked && input && (<div css={`margin-top: 0.5rem;`}>✅Commune validée</div>)}
     </div>
   )
 }
+
+export const CityList = styled.ul`
+  padding: 0;
+  background: #F5F5FE;
+  border-radius: 0 0 5px 5px;
+  border: 1px solid #DFDFF0;
+  list-style-type: none;
+  li {
+    padding: 8px 24px 8px 35px;
+    line-height: 1.2rem;
+    &::before, &.selected::before {
+      content: '';
+    }
+    &:not(:first-child):hover, &.selected {
+      background: rgba(0, 0, 145, 0.10);
+      color: var(--color);
+    }
+    &:not(:first-child):hover::before, &.selected::before {
+      content: '✔';
+      margin-left: -20px;
+      margin-right: 7px;
+    }
+    &:not(:first-child) {
+      cursor: pointer;
+    }
+  }
+`
