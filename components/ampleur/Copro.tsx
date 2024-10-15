b3324ee449d679a61bc710af9c5c65c224411e22
Mael <laem@kont.me>
2024-10-15 17:11:25 +0200

    Nombreux changements pour copro et on commence écoptz

--- components/ampleur/Copro.tsx
+++ components/ampleur/Copro.tsx
@@ -11,76 +11,94 @@ import { encodeSituation } from '../publicodes/situationUtils'
 import Value from '../Value'
 import checkIcon from '@/public/check.svg'
 
-export default function Copro({ 
-  engine, 
-  situation, 
+export default function Copro({
+  engine,
+  situation,
   setSearchParams,
   answeredQuestions,
-  expanded
- }) {
-  const dottedName = 'ampleur . prime individuelle copropriété';
+  expanded,
+}) {
+  const dottedName = 'ampleur . prime individuelle copropriété'
   const evaluation = compute('ménage . revenu . classe', engine, rules)
   const isTrèsModeste = evaluation.value === 'très modeste'
 
   return (
-    <AideAmpleur {...{
-      engine,
-      dottedName,
-      setSearchParams,
-      answeredQuestions,
-      situation,
-      expanded
-    }}>
+    <AideAmpleur
+      {...{
+        engine,
+        dottedName,
+        setSearchParams,
+        answeredQuestions,
+        situation,
+        expanded,
+      }}
+    >
       <Card $background="#f7f8f8">
-        <p
-        >
-          En tant que ménage{' '}<Value
+        <p>
+          En tant que ménage{' '}
+          <Value
             {...{
               engine,
               situation,
               dottedName: 'ménage . revenu . classe',
               state: 'prime-black',
             }}
-          />, vous êtes éligible à une prime individuelle de{' '}<Value
+          />
+          , vous êtes éligible à une prime individuelle de{' '}
+          <Value
             {...{
               engine,
               situation,
               dottedName: 'ampleur . prime individuelle copropriété . montant',
               state: 'prime-black',
             }}
-          />.
+          />
+          .
         </p>
       </Card>
-      { expanded && (
+      {expanded && (
         <>
           <h3>Comment est calculée l'aide ?</h3>
-          <p>L'aide MaPrimeRénov' Copropriété finance 30 % ou 45 % du montant des travaux selon l'ambition de la rénovation énergétique, 
-            dans un plafond de 25 000 € par logement :</p>
-          <ul>
-            <li>30 % pour une rénovation permettant un gain énergétique d'au moins 35 %</li>
-            <li>45 % pour une rénovation énergétique permettant un gain énergétique d'au moins 50 %.</li>
-          </ul>
-          <p css={`margin-top: 1rem;`}>En complément, il existe un bonus de 10 % en cas de sortie du statut de passoire énergétique (les immeubles classés <DPELabel index="5" /> ou <DPELabel index="6" /> qui atteignent au moins la classe <DPELabel index="3" /> après travaux) et des primes individuelles pour les copropriétaires aux ressources modestes (1 500 €) et très modestes (3 000 €).</p>
-          <p>Les copropriétés dites « fragiles » peuvent bénéficier d'un bonus de 20 % du montant des travaux (sous condition de l'obtention des CEE par l'Anah). Il s'agit des copropriétés qui ont un taux d'impayés de charges de copropriété d'au moins 8 % du budget voté de l'année N-2.</p>
+
+          <p>
+            L’aide MaPrimeRénov’ Copropriété finance 30 % ou 45 % du montant des
+            travaux selon l’ambition de la rénovation énergétique, dans un
+            plafond de 25 000 € par logement.
+          </p>
+          <p>
+            Une prime supplémentaire est destinée aux copropriétaires aux
+            ressources modestes et très modestes. Le montant de cette prime est
+            respectivement de 1 500 € et de 3 000 €.
+          </p>
+
           <h3>Les principales conditions d'éligibilité ?</h3>
           <div
-            css={`list-style-image: url(${checkIcon.src}); li { margin: 1rem 0; ul {list-style-image: none;}}`}
+            css={`
+              list-style-image: url(${checkIcon.src});
+              li {
+                margin: 1rem 0;
+                ul {
+                  list-style-image: none;
+                }
+              }
+            `}
             dangerouslySetInnerHTML={{
               __html: rules[dottedName].conditionsEligibilitesHTML,
             }}
           />
+          <h3>Notre parcours dédié aux copropriétés</h3>
           <p>
-            Nous avons mis en place un parcours dédié pour les copropriétés. Il
-            vous permettra de <BlueEm>vérifier en quelques minutes</BlueEm> si
-            votre copro est éligible et le montant de votre prime individuelle
-            additionnelle.
-          </p>
-          <p>
-            Le lien s'ouvrira dans un nouvel onglet.{' '}
-            <BlueEm>Vous ne perdrez pas votre simulation actuelle</BlueEm>.
+            Simulez l'éligibilité de votre copropriété à MaPrimeRénov'
+            Coproprieté grace a ce simulateur.
           </p>
           <CTAWrapper $justify="left">
-            <CTA>
+            <CTA
+              $fontSize="normal"
+              $importance="secondary"
+              css={`
+                padding: 0.5rem 0;
+              `}
+            >
               <Link
                 target="_blank"
                 href={
@@ -88,32 +106,10 @@ export default function Copro({
                   new URLSearchParams(encodeSituation(situation))
                 }
               >
-                Tester le parcours copro
+                ➞ &nbsp;Simuler MaPrimeRénov' copropriété
               </Link>
             </CTA>
           </CTAWrapper>
-          { false && (
-            <>
-              <InformationBlock>
-                <div
-                  dangerouslySetInnerHTML={{
-                    __html: rules[dottedName].informationsUtilesHtml,
-                  }}
-                />
-              </InformationBlock>
-              <PaymentTypeBlock>
-                <Avance
-                  {...{
-                    engine,
-                    rules,
-                    situation,
-                    choice,
-                    exampleSituation,
-                  }}
-                />
-              </PaymentTypeBlock>
-            </>
-          )}
         </>
       )}
     </AideAmpleur>
