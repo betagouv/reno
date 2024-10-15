e9821d03d0766b210a707b8eecf5a8171adb23b9
Mael <laem@kont.me>
2024-10-15 17:30:57 +0200

    Améliorations du PTZ

--- components/ampleur/EcoPTZ.tsx
+++ components/ampleur/EcoPTZ.tsx
@@ -9,12 +9,12 @@ import checkIcon from '@/public/check.svg'
 import Image from 'next/image'
 import { roundToThousands } from '../utils'
 
-export default function EcoPTZ({ 
+export default function EcoPTZ({
   engine,
   situation,
   setSearchParams,
-  answeredQuestions, 
-  expanded
+  answeredQuestions,
+  expanded,
 }) {
   const dottedName = 'PTZ'
   const choice = getAmpleurDPEChoice(situation)
@@ -27,22 +27,46 @@ export default function EcoPTZ({
     ...situation,
   }
   return (
-    <AideAmpleur {...{
+    <AideAmpleur
+      {...{
         engine,
         dottedName,
         setSearchParams,
         answeredQuestions,
         situation,
-        expanded
+        expanded,
       }}
     >
-      { expanded && (
+      {' '}
+      {expanded && (
+        <p>
+          L'éco-PTZ est particulièrement adapté pour{' '}
+          <a href="https://www.service-public.fr/particuliers/vosdroits/F36448">
+            {' '}
+            couvrir le reste à charge des travaux{' '}
+          </a>{' '}
+          du parcours MaPrimeRénov' accompagné.
+        </p>
+      )}
+      {expanded && (
         <>
           <h3>Comment est calculée l'aide ?</h3>
-          <p>L'éco-prêt à taux zéro est accessible à tous, sans condition de ressources.</p>
-          <p>Le montant maximum du prêt dépend du nombre de travaux engagés, pour un montant maximal de <Key $state="prime-black">50 000 €</Key>.</p>
+          <p>
+            L'éco-prêt à taux zéro est accessible à tous, sans condition de
+            ressources.
+          </p>
+          <p>
+            Vous pouvez emprunter jusqu'à{' '}
+            <Key $state="prime-black">50 000 €</Key> pour une rénovation
+            d'ampleur.
+          </p>
           <p>La durée du remboursement est de 20 ans maximum.</p>
-          <Card $background="#f7f8f8" css={`padding: 1rem;`}>
+          <Card
+            $background="#f7f8f8"
+            css={`
+              padding: 1rem;
+            `}
+          >
             <div
               css={`
                 display: flex;
@@ -51,34 +75,32 @@ export default function EcoPTZ({
             >
               <p>
                 Par rapport à un prêt à la consommation de 50 000 € affecté aux
-                travaux à un taux de 5 % sur 20 ans, l'éco-PTZ peut vous faire économiser{' '}
+                travaux à un taux de 5 % sur 20 ans, l'éco-PTZ peut vous faire
+                économiser{' '}
                 <Key $state="prime-black">
                   <a href="https://www.lafinancepourtous.com/outils/calculateurs/calculateur-de-credit-immobilier/">
                     30 000 € d'intérêts
                   </a>
-                </Key>.
+                </Key>
+                .
               </p>
             </div>
           </Card>
           <h3>Les principales conditions d'éligibilité ?</h3>
           <div
-            css={`list-style-image: url(${checkIcon.src}); li { margin: 1rem 0; ul {list-style-image: none;}}`}
+            css={`
+              list-style-image: url(${checkIcon.src});
+              li {
+                margin: 1rem 0;
+                ul {
+                  list-style-image: none;
+                }
+              }
+            `}
             dangerouslySetInnerHTML={{
               __html: rules[dottedName].conditionsEligibilitesHTML,
             }}
-          />      
-          <InformationBlock>
-            <div
-              dangerouslySetInnerHTML={{
-                __html: rules[dottedName].informationsUtilesHtml,
-              }}
-            />
-          </InformationBlock>
-          <p>
-            Vous pouvez emprunter jusqu'à 50 000 € sur 20 ans sans devoir
-            rembourser d'intérêts pour financer vos travaux de rénovation
-            energétique.
-          </p>
+          />
           <PaymentTypeBlock>
             <p>Le prêt sera à rembourser mensuellement.</p>
           </PaymentTypeBlock>
@@ -94,7 +116,7 @@ export default function EcoPTZ({
               </a>
               .
             </p>
-          </AideCTA> 
+          </AideCTA>
         </>
       )}
     </AideAmpleur>
