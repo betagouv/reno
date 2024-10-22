963aa267e7d661b0ef8d60e7e9f09512640b291f
Mael <laem@kont.me>
2024-10-22 17:33:25 +0200

    Deux iframe splutôt qu'un

--- app/stats/Stats.tsx
+++ app/stats/Stats.tsx
@@ -2,19 +2,29 @@
 
 export default function () {
   return (
-    <section>
-      <div
-        id="widgetIframe"
-        css={`
-          iframe {
-            height: 900px;
-          }
-        `}
-      >
+    <section
+      css={`
+        > div {
+          margin: 1rem 0;
+        }
+      `}
+    >
+      <div id="widgetIframe">
         <iframe
           width="100%"
-          height="100%"
-          src="https://stats.beta.gouv.fr/index.php?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=101&period=day&date=yesterday"
+          height="246px"
+          src="https://stats.beta.gouv.fr/index.php?forceView=1&viewDataTable=graphEvolution&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=101&period=day&date=yesterday"
+          scrolling="yes"
+          frameborder="0"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
+      </div>
+      <div id="widgetIframe">
+        <iframe
+          width="100%"
+          height="436px"
+          src="https://stats.beta.gouv.fr/index.php?forceView=1&viewDataTable=sparklines&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=get&idSite=101&period=day&date=yesterday"
           scrolling="yes"
           frameborder="0"
           marginheight="0"
