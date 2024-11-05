f202eb72dd035de4531b8acfb201fdf75813e826
Mael <laem@kont.me>
2024-11-05 11:24:29 +0100

    Changement du fichier de publication du modèle publicodes

--- lib/writePublicodesJson.ts
+++ lib/writePublicodesJson.ts
@@ -1,6 +1,7 @@
 import { writeFileSync } from 'fs'
 import json from '@/app/règles/rules'
 
+const filename = 'mesaidesreno.model.json' // this is a standard for the publicodes import tool https://github.com/publicodes/tools
 export default function writePublicodesJson() {
-  return writeFileSync('./public/mesaidesreno.json', JSON.stringify(json))
+  return writeFileSync('./public/' + filename, JSON.stringify(json))
 }
