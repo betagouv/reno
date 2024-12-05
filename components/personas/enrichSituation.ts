6e7dc516512955d56d8920b17d4c1ea38c59424c
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-12-05 10:59:08 +0100

    Déplacement de la fonction getCommune pour pouvoir l'utiliser dans l'API (composant server)

--- components/personas/enrichSituation.ts
+++ components/personas/enrichSituation.ts
@@ -15,3 +15,19 @@ export default async function enrichSituation(situation) {
   const éligibilité = await request.json()
   return { ...situation, ...éligibilité }
 }
+
+export async function getCommune(situation, type) {
+  if (
+    situation &&
+    ['ménage . commune', 'logement . commune'].includes(type) &&
+    situation[type]
+  ) {
+    const response = await fetch(
+      `https://geo.api.gouv.fr/communes?code=${situation[type].toString().replace(/"/g, '')}`,
+    )
+    const json = await response.json()
+
+    return json[0]
+  }
+  return null
+}
