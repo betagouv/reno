b3907d6b9ec620363821626cbdea7e806ef10e04
Morgan Merzouk <morgan.merzouk@gmail.com>
2024-12-18 10:41:15 +0100

    Correction du build

--- app/integration/page.tsx
+++ app/integration/page.tsx
@@ -1,6 +1,6 @@
-import css from '@/components/css/convertToJs'
 import Integration from '@/components/Integration'
 import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
+import { Suspense } from 'react'
 
 export const description = `Vous pouvez intégrer le simulateur Mes Aides Réno sur votre site Web en trois clics.`
 
@@ -12,13 +12,8 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <main
-      style={css`
-        background: white;
-        padding-top: calc(1.5vh + 1.5vw);
-      `}
-    >
+    <Suspense>
       <Integration />
-    </main>
+    </Suspense>
   )
 }
