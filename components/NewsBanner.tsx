f074529042e74e6f0a5c7da6d80a686c8c64440d
Mael <laem@kont.me>
2024-12-18 12:20:32 +0100

    Nouvelle bannière 2025

---
+++ components/NewsBanner.tsx
@@ -0,0 +1,47 @@
+'use client'
+import Link from 'next/link'
+import { NewsBannerWrapper } from './NewsBannerUI'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import crossIcon from '@/public/remix-close-empty.svg'
+import { useState } from 'react'
+
+export default function NewsBanner() {
+  const [closed, setClosed] = useState(false)
+  if (closed) return null
+  return (
+    <motion.div
+      role="alert"
+      initial={{ y: -30, scale: 1, opacity: 0 }}
+      animate={{ y: 0, scale: 1, opacity: 1 }}
+      transition={{
+        type: 'spring',
+        stiffness: 120,
+        damping: 20,
+      }}
+    >
+      <NewsBannerWrapper>
+        <p>
+          Vous utilisez la version 2025.{' '}
+          <Link href="/blog">En savoir plus</Link>
+        </p>
+        <Image
+          src={crossIcon}
+          alt="Icône d'une croix"
+          css={`
+            filter: grayscale(1);
+            width: 1.4rem;
+            height: auto;
+            position: absolute;
+            top: 0.05rem;
+            right: 0.1rem;
+            cursor: pointer;
+          `}
+          onClick={() => {
+            setClosed(true)
+          }}
+        />
+      </NewsBannerWrapper>
+    </motion.div>
+  )
+}
---
+++ components/NewsBannerUI.tsx
@@ -0,0 +1,20 @@
+'use client'
+import styled from 'styled-components'
+
+export const NewsBannerWrapper = styled.section`
+  position: relative;
+  margin: 1rem auto;
+  width: 16rem;
+  background: var(--lightestColor);
+  text-align: center;
+  padding: 0.4rem;
+  padding-right: 2rem;
+  border-radius: 0.6rem;
+  p {
+    display: inline-block;
+    margin: 0 auto;
+    line-height: 1.2rem;
+  }
+  font-size: 90%;
+  margin-top: -1vh;
+`
