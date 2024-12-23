8cb705b1265b8ebb92603b01a5a690153835738c
Mael <laem@kont.me>
2024-12-23 15:25:35 +0100

    Lien 2024 directement dans la bannière, qui se transforme

--- components/NewsBanner.tsx
+++ components/NewsBanner.tsx
@@ -1,10 +1,12 @@
 'use client'
 import Link from 'next/link'
-import { NewsBannerWrapper } from './NewsBannerUI'
+import { NewsBannerWrapper, YellowCard } from './NewsBannerUI'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import crossIcon from '@/public/remix-close-empty.svg'
 import { useState } from 'react'
+import { Card } from './UI'
+import checkIcon from '@/public/check-green.svg'
 
 export default function NewsBanner() {
   const [closed, setClosed] = useState(false)
@@ -21,9 +23,36 @@ export default function NewsBanner() {
       }}
     >
       <NewsBannerWrapper>
-        <p>💫 Vous utilisez la version 2025. </p>
         <p>
-          <Link href="/blog/aides-renovation-2025">En savoir plus</Link>
+          <Image
+            src={checkIcon}
+            width="30"
+            alt="Icône case cochée"
+            css={`
+              vertical-align: sub;
+              width: 1rem;
+              height: auto;
+              margin-right: 0.2rem;
+              filter: grayscale(1);
+              display: none;
+            `}
+          />
+          <small>Vous utilisez la version 2025. </small>
+        </p>
+        <YellowCard>
+          <Link href="/blog/aides-renovation-2025">
+            <span>💫</span>
+            <p>
+              Quels changements <br /> au 1er janvier 2025 ?
+            </p>
+          </Link>
+        </YellowCard>
+        <p>
+          <small>
+            <a href="https://2024.mesaidesreno.beta.gouv.fr" target="_blank">
+              Revenir à la version 2024
+            </a>
+          </small>
         </p>
         <Image
           src={crossIcon}
@@ -33,9 +62,11 @@ export default function NewsBanner() {
             width: 1.4rem;
             height: auto;
             position: absolute;
-            top: 0.05rem;
-            right: 0.1rem;
+            top: 1rem;
+            right: 0.9rem;
             cursor: pointer;
+            background: white;
+            z-index: 10;
           `}
           onClick={() => {
             setClosed(true)
--- components/NewsBannerUI.tsx
+++ components/NewsBannerUI.tsx
@@ -1,14 +1,14 @@
 'use client'
 import styled from 'styled-components'
+import { Card } from './UI'
 
-export const NewsBannerWrapper = styled.section`
+export const NewsBannerWrapper = styled.div`
   position: relative;
   margin: 1rem auto;
-  width: 17rem;
-  background: var(--lightestColor);
-  text-align: center;
+  width: 19rem;
+  max-width: 95%;
   padding: 0.4rem;
-  padding-right: 2rem;
+  padding-right: 1.4rem;
   padding-left: 1rem;
   border-radius: 0.6rem;
   p {
@@ -18,4 +18,32 @@ export const NewsBannerWrapper = styled.section`
   }
   font-size: 90%;
   margin-top: -1vh;
+  p {
+    width: 100%;
+    > small > a {
+      text-align: right;
+      display: block;
+      text-decoration: none;
+    }
+  }
+`
+
+export const YellowCard = styled(Card)`
+  margin-top: 0;
+  margin-bottom: 0;
+  background: white;
+  border: 2px solid #fdd888;
+  &:hover {
+    background: #fdd88830;
+  }
+  > a  {
+    > span {
+      font-size: 200%;
+      margin-right: 1rem;
+    }
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
+  }
 `
