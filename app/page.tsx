840e3e32e8c91afffa5d4eaa18b7181023bb2c3b
Mael <laem@kont.me>
2024-12-26 15:25:08 +0100

    2025 dans les meta de la page d'accueil

--- app/page.tsx
+++ app/page.tsx
@@ -22,10 +22,10 @@ import HomepageTalkAboutUs from './HomepageTalkAboutUs'
 import HomepageTestimonies from './HomepageTestimonies'
 import NewsBanner from '@/components/NewsBanner'
 
-export const description = `Calculez les aides MaPrimeRénov' 2024 pour la rénovation de votre logement.`
+export const description = `Calculez les aides MaPrimeRénov' 2025 pour la rénovation de votre logement.`
 
 export const metadata: Metadata = {
-  title: 'Mes aides réno 2024',
+  title: 'Mes aides réno 2025',
   description,
   openGraph: { images: ['/jaquette.png'] },
 }
