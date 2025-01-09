7089b438177725232ef82b422893ce10171eb1eb
Mael <laem@kont.me>
2025-01-09 11:02:21 +0100

    MAJ meta /simulation

--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -1,10 +1,10 @@
 import rules from '@/app/règles/rules'
 import Form from './Form'
 
-const description = `Calculez les aides Ma Prime Rénov' 2024 pour la rénovation de votre logement.`
+const description = `Calculez les aides Ma Prime Rénov' 2025 pour la rénovation de votre logement.`
 
 export const metadata: Metadata = {
-  title: 'Aides réno 2024',
+  title: 'Mes Aides réno : simulation 2025',
   description,
   alternates: {
     canonical: '/simulation',
