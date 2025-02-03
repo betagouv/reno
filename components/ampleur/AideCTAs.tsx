394f7f7e62858300f7f0cf48d2f37e1f66d62b74
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-02-03 17:10:39 +0100

    Amélioration du bouton pour voir le détail d'une aide

--- components/ampleur/AideCTAs.tsx
+++ components/ampleur/AideCTAs.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import rules from '@/app/règles/rules'
-import { CTAWrapper, CTA } from '../UI'
+import { CTA } from '../UI'
 import {
   encodeDottedName,
   encodeSituation,
   getSituation,
 } from '../publicodes/situationUtils'
-
-import eyeIcon from '@/public/eye.svg'
+import iconCalculator from '@/public/calculator.svg'
+import iconFlecheDroite from '@/public/fleche-droite-bleue.svg'
 import { omit } from '@/components/utils'
 import { useSearchParams } from 'next/navigation'
 import Image from 'next/image'
@@ -52,113 +52,55 @@ export default function AideCTAs({
       true,
     )
 
-  const addToSynthese = (dottedName) => {
-    const encodedSituation = encodeSituation(
-      {
-        ...situation,
-        'ampleur.synthèse':
-          (searchParams['ampleur.synthèse']
-            ? searchParams['ampleur.synthèse'] + ','
-            : '') +
-          '"' +
-          encodeDottedName(dottedName) +
-          '"',
-      },
-      false,
-      answeredQuestions,
-    )
-
-    setSearchParams(encodedSituation, 'push', false)
-  }
-
-  const removeFromSynthese = (dottedName) => {
-    const newSynthese = searchParams['ampleur.synthèse']
-      .split(',')
-      .filter((item) => item != '"' + encodeDottedName(dottedName) + '"')
-    const encodedSituation = encodeSituation(
-      newSynthese.length > 0
-        ? {
-            ...situation,
-            'ampleur.synthèse': newSynthese,
-          }
-        : omit(['ampleur . synthèse'], situation),
-      false,
-      answeredQuestions,
-    )
-
-    setSearchParams(encodedSituation, 'push', true)
-  }
-
-  const isSelected = searchParams['ampleur.synthèse']
-    ?.split(',')
-    .find((item) => item === '"' + encodeDottedName(dottedName) + '"')
-
   return (
-    <CTAWrapper
-      $justify="center"
+    <CTA
+      $fontSize="normal"
+      $importance="emptyBackground"
       css={`
-        flex-wrap: wrap;
-        flex-direction: ${expanded ? 'row' : 'column'};
-        div {
-          ${!expanded && 'width: 100%;'}
-          text-align: center;
-        }
+        margin-bottom: 1rem;
       `}
     >
-      <CTA $fontSize="normal" $importance="emptyBackground">
-        <Link
-          href={expanded ? backUrl : detailUrl}
-          onClick={() =>
-            !expanded &&
-            push(['trackEvent', 'Simulateur Principal', 'Détails', dottedName])
-          }
-        >
-          {expanded ? (
-            <>← Revenir aux aides</>
-          ) : (
-            <span
+      <Link
+        href={expanded ? backUrl : detailUrl}
+        onClick={() =>
+          !expanded &&
+          push(['trackEvent', 'Simulateur Principal', 'Détails', dottedName])
+        }
+      >
+        {expanded ? (
+          <>← Revenir aux aides</>
+        ) : (
+          <span
+            css={`
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            `}
+          >
+            {['MPR . accompagnée', 'denormandie'].includes(dottedName) ? (
+              <>
+                <Image
+                  src={iconCalculator}
+                  alt="icone calculatrice"
+                  css={`
+                    margin-right: 0.5rem;
+                  `}
+                />
+                Calculer le montant d'aides
+              </>
+            ) : (
+              <>En savoir plus sur {rules[dottedName].titre}</>
+            )}
+            <Image
+              src={iconFlecheDroite}
+              alt="icone calculatrice"
               css={`
-                display: flex;
-                justify-content: center;
-                align-items: center;
+                margin-left: 0.5rem;
               `}
-            >
-              <Image
-                src={eyeIcon}
-                alt="icon oeil"
-                css={`
-                  margin-right: 0.5rem;
-                `}
-              />
-              Voir le détail sur l'aide
-            </span>
-          )}
-        </Link>
-      </CTA>
-      {/* <CTA css={`
-          ${isSelected && `
-            background: rgba(190, 242, 197, 0.20);
-            border: 1px dashed var(--validColor);
-            color: var(--validColor);  
-          `}
-        `} $fontSize="normal">
-        <button
-          onClick={() => !isSelected && addToSynthese(dottedName)}>
-            { isSelected ? "✔ Ajouté" : "+ Ajouter" } à ma synthèse
-        </button>
-      </CTA>
-      {isSelected && 
-        <CTA css={`
-          color: #721c24;
-          background-color: #f8d7da;
-          border: 1px dashed #721c24;
-        `} $fontSize="normal">
-          <button
-            onClick={() => removeFromSynthese(dottedName)}>
-              ✖ Retirer de ma synthèse
-          </button>
-        </CTA>
-      } */}
-    </CTAWrapper>
+            />
+          </span>
+        )}
+      </Link>
+    </CTA>
   )
 }
