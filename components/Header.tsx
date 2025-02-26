e4a90f817079746930f3450fbd9594cf85543c00
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-02-26 11:51:57 +0100

    Correction d'un bug sur l'affichage du header

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ import { CTA } from './UI'
 export default function Header() {
   const isInIframe = useIsInIframe()
   const pathname = usePathname()
-  if (pathname !== '/module') return
+  if (pathname.startsWith('/module/')) return
 
   return isInIframe ? (
     <LogoCompact />
