f9a10d0ab3e6329c152a628b0168143e262ccd45
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-03-26 12:24:22 +0100

    Debug API

--- app/api/dpe/route.ts
+++ app/api/dpe/route.ts
@@ -12,32 +12,25 @@ export async function GET(request: Request) {
 
   const url = `https://observatoire-dpe-audit.ademe.fr/pub/dpe/${dpeNumber}/xml`
 
-  try {
-    const response = await fetch(url, { mode: 'no-cors' })
-    if (!response.ok) {
-      return new Response(
-        JSON.stringify({
-          error: response.ok,
-          status: response.status,
-          statusText: response.statusText,
-        }),
-        {
-          status: response.status,
-          headers: { 'Content-Type': 'application/json' },
-        },
-      )
-    }
-
-    const blob = await response.blob()
-    const buffer = await blob.arrayBuffer()
-    return new Response(Buffer.from(buffer), {
-      status: 200,
-      headers: { 'Content-Type': 'application/xml' },
-    })
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
+  const response = await fetch(url, { mode: 'no-cors' })
+  if (!response.ok) {
+    return new Response(
+      JSON.stringify({
+        error: response.ok,
+        status: response.status,
+        statusText: response.statusText,
+      }),
+      {
+        status: response.status,
+        headers: { 'Content-Type': 'application/json' },
+      },
+    )
   }
+
+  const blob = await response.blob()
+  const buffer = await blob.arrayBuffer()
+  return new Response(Buffer.from(buffer), {
+    status: 200,
+    headers: { 'Content-Type': 'application/xml' },
+  })
 }
