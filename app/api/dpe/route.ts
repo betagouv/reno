e21d18ec710be5ce3958acea2bbd974fb654d62f
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-03-26 12:15:34 +0100

    Debug API

--- app/api/dpe/route.ts
+++ app/api/dpe/route.ts
@@ -15,7 +15,7 @@ export async function GET(request: Request) {
   try {
     const response = await fetch(url)
     if (!response.ok) {
-      return new Response(JSON.stringify({ error: error.message }), {
+      return new Response(JSON.stringify({ error: response }), {
         status: response.status,
         headers: { 'Content-Type': 'application/json' },
       })
