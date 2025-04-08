53551e7b636be55a0ba05c979a6f891da5dc398f
Mael <laem@kont.me>
2025-04-08 11:21:55 +0200

    Petit easter egg

--- app/blog/BlogImage.tsx
+++ app/blog/BlogImage.tsx
@@ -5,24 +5,35 @@ import illustrationMobile from '@/public/chat.svg'
 import Image from 'next/image'
 import { useMediaQuery } from 'usehooks-ts'
 import css from '@/components/css/convertToJs'
+import { useState } from 'react'
 
 export default function BlogImage() {
   const isMobile = useMediaQuery('(max-width: 800px)', {
     initializeWithValue: false,
   })
 
+  const [color, setColor] = useState(true)
+
   const alt = !isMobile
     ? 'Une personne consulte confortablement sur sa tablette un blog sur les aides à la rénovation énergétique'
     : 'Un chat tranquille sur un sol au bon confort thermique'
 
-  const mobileStyle = `display: block; width: 4rem; height: auto; position: absolute;top:10rem; right: 0`
+  const mobileStyle = `display: block; width: 4rem; height: auto; position: absolute;top:10rem; right: 0; cursor: pointer`
 
   return (
     <Image
       style={{ margin: '.6rem 3rem' }}
       src={!isMobile ? illustrationBlog : illustrationMobile}
-      style={isMobile ? css(mobileStyle) : ''}
+      style={
+        isMobile
+          ? {
+              ...css(mobileStyle),
+              filter: color ? `hue-rotate(${color}deg)` : '',
+            }
+          : ''
+      }
       alt={alt}
+      onClick={() => setColor((color) => Math.random() * 180)}
     />
   )
 }
