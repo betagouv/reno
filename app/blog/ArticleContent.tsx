ffca68dc30271f833dc1a289288533c1a85edda9
Mael <laem@kont.me>
2025-04-14 15:16:27 +0200

    Imports et logs inutiles

--- app/blog/ArticleContent.tsx
+++ app/blog/ArticleContent.tsx
@@ -1,20 +1,14 @@
-//import { useMDXComponent } from 'next-contentlayer2/hooks'
 import { mdxComponents } from './mdxComponents'
-//import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote'
 
 import { MDXRemote } from 'next-mdx-remote-client/rsc'
 
+import ExampleIframe from '@/app/module/ExampleIframe.tsx'
 import CoproCTA from '@/components/CoproCTA'
 import InterdictionTable from '@/components/InterdictionTable'
-import ExampleIframe from '@/app/module/ExampleIframe.tsx'
 
 export default function ArticleContent({ post }) {
-  //  const MDXContent = useMDXComponent(post.body.code)
-  // return <MDXContent components={mdxComponents} />
-
   const { source, slug } = post
 
-  console.log('cyan', slug, source)
   return (
     <MDXRemote
       source={source}
