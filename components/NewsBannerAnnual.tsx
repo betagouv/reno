4dbde8517d0f8827e781dc6749a8cc023bb20f6c
Mael <laem@kont.me>
2025-04-14 16:14:43 +0200

    Reprise de la logique de nouveauté 2025 pour les brèves

--- components/NewsBannerAnnual.tsx
+++ components/NewsBannerAnnual.tsx
@@ -6,8 +6,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import { NewsBannerWrapper, YellowCard } from './NewsBannerUI'
-import news from '@/articles/brèves.yaml'
-const lastNews = news[0]
 
 export default function NewsBanner() {
   const [closed, setClosed] = useState(false)
@@ -25,12 +23,36 @@ export default function NewsBanner() {
     >
       <NewsBannerWrapper>
         <p>
-          <span>💫&nbsp;</span>
-          <small>Nouveauté !</small>
+          <Image
+            src={checkIcon}
+            width="30"
+            alt="Icône case cochée"
+            css={`
+              vertical-align: sub;
+              width: 1rem;
+              height: auto;
+              margin-right: 0.2rem;
+              filter: grayscale(1);
+              display: none;
+            `}
+          />
+          <small>Vous utilisez la version 2025. </small>
         </p>
         <YellowCard>
-          <Link href="/breves">{lastNews.titre}</Link>
+          <Link href="/blog/aides-renovation-2025">
+            <span>💫</span>
+            <p>
+              Quels changements <br /> au 1er janvier 2025 ?
+            </p>
+          </Link>
         </YellowCard>
+        <p>
+          <small>
+            <a href="https://2024.mesaidesreno.beta.gouv.fr" target="_blank">
+              Revenir à la version 2024
+            </a>
+          </small>
+        </p>
         <Image
           src={crossIcon}
           alt="Icône d'une croix"
