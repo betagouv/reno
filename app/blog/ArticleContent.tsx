6e78489ce2340ca2f09ac7858b3f50287a1b847c
Mael <laem@kont.me>
2025-04-14 13:34:46 +0200

    Passage à la nouvelle lib mdx-remote car l'originale plante sur next15.2

--- app/blog/ArticleContent.tsx
+++ app/blog/ArticleContent.tsx
@@ -1,18 +1,23 @@
-'use client'
 //import { useMDXComponent } from 'next-contentlayer2/hooks'
-//import { mdxComponents } from './mdxComponents'
-//
-import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote'
+import { mdxComponents } from './mdxComponents'
+//import { MDXRemote, type MDXRemoteSerializeResult } from 'next-mdx-remote'
 
-//import { MDXRemote } from 'next-mdx-remote/rsc'
+import { MDXRemote } from 'next-mdx-remote-client/rsc'
 
-export default async function ArticleContent({ post }) {
+export default function ArticleContent({ post }) {
   //  const MDXContent = useMDXComponent(post.body.code)
   // return <MDXContent components={mdxComponents} />
 
   const { source, slug } = post
 
   console.log('cyan', slug, source)
-  return <MDXRemote {...post.serialized} />
-  //return <MDXRemote source={`# salut`}  />
+  return (
+    <MDXRemote
+      source={source}
+      components={mdxComponents}
+      options={{
+        parseFrontmatter: true,
+      }}
+    />
+  )
 }
