50c96a0bd1aa8c1c47874d0956a31e7f1fa8c650
Mael <laem@kont.me>
2025-05-12 12:32:04 +0200

    Les params sont async même dans les meta

--- app/dpe/[numDpe]/page.tsx
+++ app/dpe/[numDpe]/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import DPEPage from '@/components/dpe/DPEPage'
-export async function generateMetadata({ params }) {
+export async function generateMetadata(props) {
+  const params = await props.params
   return {
     title: `Les informations importantes pour le DPE ${params.numDpe}`,
     description: `Pour le DPE n° ${params.numDpe} : quelles sont les aides à la rénovation? y a-t-il une interdiction de location? quel est l'impact sur le prix à la revente?`,
