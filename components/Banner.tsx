3088695ef8326f44abf9bc06b464dc0d9da537fe
Mael <laem@kont.me>
2025-06-19 19:13:59 +0200

    Croix pour fermer stockée dans le LS

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,10 +1,22 @@
 'use client'
-import { CTA } from '@/components/UI'
+import closeIcon from '@/public/close-line.svg'
 import informationIcon from '@/public/information.svg'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
+import { useLocalStorage } from 'usehooks-ts'
 
+const ls = 'bannière-guichet-juin-2025'
 export default function Banner({}) {
+  const [bannerRead, setBannerRead] = useState()
+
+  useEffect(() => {
+    const value = localStorage.getItem(ls)
+    setBannerRead(value || false)
+  }, [setBannerRead, bannerRead])
+
+  if (bannerRead == null) return null
+  if (bannerRead) return null
   return (
     <Section>
       <div>
@@ -15,6 +27,15 @@ export default function Banner({}) {
             temporairement suspendu
           </strong>
         </header>
+
+        <button
+          onClick={() => {
+            localStorage.setItem(ls, true)
+            setBannerRead(true)
+          }}
+        >
+          <Image src={closeIcon} alt="fermer la bannière" width="25" />
+        </button>
         <div>
           <p>
             La réouverture de MaPrimeRénov’ dédiée à la rénovation globale
@@ -30,7 +51,7 @@ export default function Banner({}) {
 
 const Section = styled.section`
   padding: 1rem 1.2rem;
-
+  position: relative;
   width: 100vw;
   background: #e8edff;
   color: #0063cb;
@@ -50,5 +71,16 @@ const Section = styled.section`
     width: 900px;
     max-width: 100%;
     margin: 0 auto;
+  }
+  button {
+    background: none;
+    border: none;
+    position: absolute;
+    right: 0rem;
+    top: 1rem;
+    img {
+      width: 1.4rem;
+      height: auto;
+    }
   }
 `
