5cc19fcab6f7693f7d065f720ca0dade124fe759
Mael <laem@kont.me>
2025-06-24 18:21:36 +0200

    Sauvegarde du fait que l'AvertissementSimulation a été lu (pendant 1 mois)

--- components/AvertissementSimulation.tsx
+++ components/AvertissementSimulation.tsx
@@ -1,10 +1,48 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
 import euroIcon from '@/public/euro-entouré.svg'
 
+const localStorageKey = 'avertissementSimulationLuLe'
+
+function isMoreThanOneMonthLater(date1, date2) {
+  // Parse the dates into Date objects
+  const dt1 = new Date(date1)
+  const dt2 = new Date(date2)
+
+  // Calculate the difference in months
+  const monthDiff =
+    (dt2.getFullYear() - dt1.getFullYear()) * 12 +
+    (dt2.getMonth() - dt1.getMonth())
+
+  // Check if the difference is more than one month
+  return monthDiff > 1
+}
+
+function serializeDay(date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0') // Months are zero-based
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
+
 export const useAvertissementState = () => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(undefined)
+
+  useEffect(() => {
+    const now = new Date()
+    const value = localStorage.getItem(localStorageKey)
+    if (value != undefined) {
+      const readDate = new Date(value)
+      if (isMoreThanOneMonthLater(readDate, now)) {
+        setOpen(true)
+      } else setOpen(false)
+    } else {
+      setOpen(true)
+    }
+    localStorage.setItem(localStorageKey, serializeDay(now))
+  }, [setOpen])
 
   return [open, setOpen]
 }
