e14813e0091586436176f39076c3393ed27bb560
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-07-15 15:57:19 +0200

    Modification du Header en dsfr

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ import { usePathname } from 'next/navigation'
 import styled from 'styled-components'
 import { useMediaQuery } from 'usehooks-ts'
 import Menu from './Menu'
+import { Header as DsfrHeader } from '@codegouvfr/react-dsfr/Header'
 
 export default function Header() {
   const isInIframe = useIsInIframe()
@@ -19,44 +20,58 @@ export default function Header() {
     return
 
   return (
-    <HeaderWrapper>
-      <nav>
-        <Link
-          href="/"
-          style={css`
-            text-decoration: none;
-            color: inherit;
-          `}
-          target={isInIframe ? '_blank' : undefined}
-        >
-          <div
-            style={css`
-              display: flex;
-              align-items: center;
-            `}
-          >
-            {!isMobile && <MarianneHeaderLogo />}
-            <div
-              style={css`
-                display: flex;
-                align-items: center;
-                margin-left: 1vw;
-              `}
-            >
-              <HeaderLogo src={logo} alt={logoAlt} $needsMargin={!isMobile} />
-            </div>
-          </div>
-        </Link>
-        <Menu isMobile={isMobile} />
-      </nav>
-    </HeaderWrapper>
+    <>
+      <DsfrHeader
+        brandTop={
+          <>
+            RÉPUBLIQUE
+            <br />
+            FRANCAISE
+          </>
+        }
+        homeLinkProps={{
+          href: '/',
+          title: 'Accueil - Mes Aides Réno',
+        }}
+        id="fr-header-simple-header-with-service-title-and-tagline"
+        serviceTitle={
+          <HeaderLogo
+            src={logo}
+            alt="Logo Mes Aides Réno, représentant une maison bleu blanc rouge ainsi que la marque à laquellee le service est rattaché, le visage souriant France Rénov' avec un toît en guise de couvre-chef."
+          />
+        }
+        quickAccessItems={[
+          {
+            text: 'Blog',
+            linkProps: {
+              href: '/blog',
+            },
+          },
+          {
+            text: 'Les aides',
+            linkProps: {
+              href: '/aides',
+            },
+          },
+          {
+            text: 'Contact',
+            linkProps: {
+              href: '/contact',
+            },
+          },
+          {
+            text: 'Devenir partenaire',
+            linkProps: {
+              href: '/devenir-partenaire',
+            },
+          },
+        ]}
+      />
+    </>
   )
 }
 
 const HeaderLogo = styled(Image)`
   height: 4rem;
   width: auto;
-  ${(p) => p.$needsMargin && `margin-left: 0rem;`}
 `
-export const logoAlt =
-  "Logo Mes Aides Réno, représentant une maison bleu blanc rouge ainsi que la marque à laquellee le service est rattaché, le visage souriant France Rénov' avec un toît en guise de couvre-chef."
