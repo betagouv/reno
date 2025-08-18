7a987b7cb3ba90a7ee0daf0c916a6f5c19f14b83
Mael <laem@kont.me>
2025-08-18 11:27:25 +0200

    Revert "Intégration de l'autre formulaire tally et tentative de suppression"
    This reverts commit bc90abc7c3a8fa80aefa62a93f2dea8ea063ed10.

--- components/ClassicQuestionWrapper.tsx
+++ components/ClassicQuestionWrapper.tsx
@@ -62,21 +62,10 @@ export default function ClassicQuestionWrapper({
 
   const [avertissementState, setAvertissementState] = useAvertissementState()
 
-  const tallyForm =
-    currentQuestion === 'projet . définition'
-      ? 'mKjKNk'
-      : currentQuestion ===
-            'projet . définition . catégories travaux envisagées' ||
-          currentQuestion === 'projet . définition . travaux envisagés' ||
-          currentQuestion ===
-            'projet . définition . travaux envisagés chauffage'
-        ? 'np2g2P'
-        : null
+  const tallyForm = currentQuestion === 'projet . définition' ? 'mKjKNk' : null
 
   return (
     <>
-      {' '}
-      {currentQuestion}
       <ProgressBar
         {...{
           answeredQuestions,
@@ -98,17 +87,13 @@ export default function ClassicQuestionWrapper({
           remaining,
         }}
       />
-      {tallyForm ? (
+      {tallyForm && (
         <>
           <Script src="https://tally.so/widgets/embed.js"></Script>{' '}
           <Script
             id={tallyForm}
-          >{` window.TallyConfig = { "formId": "${tallyForm}", "popup": { "emoji": { "text": "👋", "animation": "wave" }, "open": { "trigger": "exit" } } }; `}</Script>
+          >{` window.TallyConfig = { "formId": "mKjKNk", "popup": { "emoji": { "text": "👋", "animation": "wave" }, "open": { "trigger": "exit" } } }; `}</Script>
         </>
-      ) : (
-        <Script
-          id={'ensureNoTallyPopup'}
-        >{` window.TallyConfig = undefined; window.Tally=undefined; `}</Script>
       )}
       <QuestionCard>
         {!rule.type && (
