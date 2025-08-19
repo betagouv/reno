4e62ac3c242aa3cfa6cb8dc7591e2a95c0b54856
Mael <laem@kont.me>
2025-08-19 17:47:39 +0200

    CORS pour que les appels du webco hors de notre domaine fonctionnent

--- app/api/geo/route.ts
+++ app/api/geo/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server'
 
+//
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
 export async function GET(request: Request) {
   const { path } = Object.fromEntries(request.nextUrl.searchParams.entries())
   const newUrl = `https://geo.api.gouv.fr/${decodeURIComponent(path)}`
@@ -14,5 +21,5 @@ export async function GET(request: Request) {
   })
 
   const data = await response.json()
-  return NextResponse.json(data)
+  return NextResponse.json(data, { headers: corsHeaders })
 }
