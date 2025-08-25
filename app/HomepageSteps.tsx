13fb2d132d50b06b93707b6785d02ea03e2034b0
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-08-25 09:00:00 +0200

    Correction du markup sur la home: h3 -> span

--- app/HomepageSteps.tsx
+++ app/HomepageSteps.tsx
@@ -13,34 +13,33 @@ export default function HomepageSteps() {
       <ol
         className="fr-grid-row fr-grid-row--gutters fr-grid-row--middle fr-grid-row--center fr-my-5v"
         css={`
-            li {
-              display: flex;
-              flex-direction: column;
-              gap: 1em;
-              flex-start;
-              h3 {
-                text-align: center;
-              }
-              strong {
-                font-weight: 800;
-                font-size: 300%;
-                display: block;
-                text-align: center;
-              }
+          li {
+            display: flex;
+            flex-direction: column;
+            gap: 1em;
+            span {
+              text-align: center;
             }
-          `}
+            strong {
+              font-weight: 800;
+              font-size: 300%;
+              display: block;
+              text-align: center;
+            }
+          }
+        `}
       >
         <li className="fr-col-12 fr-col-md-4" key="1">
           <strong>1</strong>
-          <h3>Je réponds à un questionnaire simplifié</h3>
+          <span className="fr-h3">Je réponds à un questionnaire simplifié</span>
           <p>
-            6 questions pour évaluer votre éligibilité et estimer le montant le
-            vos aides.
+            quelques questions pour évaluer votre éligibilité et estimer le
+            montant le vos aides.
           </p>
         </li>
         <li className="fr-col-12 fr-col-md-4" key="2">
           <strong>2</strong>
-          <h3>Je découvre le montant de mes aides</h3>
+          <span className="fr-h3">Je découvre le montant de mes aides</span>
           <p>
             Et j’affine mon projet pour obtenir un montant d’enveloppe global
             pour mes travaux.
@@ -48,7 +47,7 @@ export default function HomepageSteps() {
         </li>
         <li className="fr-col-12 fr-col-md-4" key="3">
           <strong>3</strong>
-          <h3>J’exporte le résultat de ma simulation</h3>
+          <span className="fr-h3">J’exporte le résultat de ma simulation</span>
           <p>
             Pour le partager avec mon conseiller local France Rénov’, mes
             proches ou mes artisans.
