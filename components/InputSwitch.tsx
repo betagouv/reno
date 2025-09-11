78de8c90b1efd4a934536523fa2424cb3794ab07
Mael <laem@kont.me>
2025-09-11 17:34:51 +0200

    Revert "Test avec une nouvelle variable code département sans valeur: commune"
    This reverts commit f3b6d0c02c5c39de73223b17d0c1330091b42652.
    
    OK je crois que c'était un souci de answeredQuestions ! On ne disait
    jamais que le département c'était plié.

--- components/InputSwitch.tsx
+++ components/InputSwitch.tsx
@@ -89,9 +89,8 @@ export default function InputSwitch({
       const risque = (bdnb['alea_argiles'] || 'nul').toLowerCase()
       const rnb = bdnb.rnb
       const niveaux = bdnb.nb_niveau
-      const codeDépartement = bdnb['code_departement_insee']
 
-      console.log({ codeDépartement })
+      console.log({ risque })
 
       // On utilise setValueToSituation car niveaux peut être null
       // Exemple : https://api.bdnb.io/v1/bdnb/donnees/batiment_groupe_complet?batiment_groupe_id=eq.bdnb-bg-E756-YKX1-D181
@@ -105,10 +104,6 @@ export default function InputSwitch({
           'rga . zone aléa': setValueToSituation('string', risque),
           'logement . rnb': setValueToSituation('string', rnb),
           'logement . niveaux': setValueToSituation('num', niveaux),
-          'rga . code département': setValueToSituation(
-            'string',
-            codeDépartement,
-          ),
         },
         false,
         answeredQuestions,
