4b170017c9add3a0cf2984749270dae131b5e709
Mael <laem@kont.me>
2025-09-15 11:31:17 +0200

    Extrait du bloc d'infos BDNB

--- components/rga/ArgileMap.tsx
+++ components/rga/ArgileMap.tsx
@@ -9,6 +9,7 @@ import useArgileMapMarkers, {
   useRnbLayerHoverEffects,
 } from './useArgileMapMarkers'
 import MapBlock from './MapBlock'
+import ChosenBuildingInfo from './ChosenBuildingInfo'
 
 /*
  * Tel que je le comprends pour l'instant, la clef BAN désigne l'adresse.
@@ -99,61 +100,7 @@ export default function ArgileMap({ situation, setChoice }) {
           `}
         >
           {data ? (
-            <section
-              css={`
-                > div {
-                  margin-bottom: 0.3rem;
-                }
-                span {
-                  margin-right: 0.6rem;
-                }
-                width: fit-content;
-                animation: flash 1s;
-                @keyframes flash {
-                  0% {
-                    opacity: 0.2;
-                    background: #18753c;
-                  }
-                  100% {
-                    opacity: 1;
-                    background: none;
-                  }
-                }
-              `}
-            >
-              <div>
-                <span>Adresse</span>
-                <p className="fr-badge">
-                  {situation['logement . adresse'].replace(/"/g, '')}
-                </p>
-              </div>
-              <div>
-                <span>Aléa argiles</span>
-                <p className="fr-badge">{data.alea_argiles || 'Nul'}</p>
-              </div>
-              {data.annee_construction && (
-                <div>
-                  <span>Année de construction</span>
-                  <p className="fr-badge">{data.annee_construction}</p>
-                </div>
-              )}
-              {data.nb_niveau && (
-                <div>
-                  <span>Niveaux</span>
-                  <p className="fr-badge">{data.nb_niveau}</p>
-                </div>
-              )}
-              <div>
-                <small
-                  css={`
-                    color: #aaa;
-                  `}
-                >
-                  Données issues du <a href="https://rnb.beta.gouv.fr/">RNB</a>{' '}
-                  et de la <a href="https://bdnb.io/">BDNB</a>
-                </small>
-              </div>
-            </section>
+            <ChosenBuildingInfo {...{ situation, data }} />
           ) : (
             <p>⏳️ En attente des données du bâtiment...</p>
           )}
---
+++ components/rga/ChosenBuildingInfo.tsx
@@ -0,0 +1,59 @@
+export default function ChosenBuildingInfo({ situation, data }) {
+  return (
+    <section
+      css={`
+        > div {
+          margin-bottom: 0.3rem;
+        }
+        span {
+          margin-right: 0.6rem;
+        }
+        width: fit-content;
+        animation: flash 1s;
+        @keyframes flash {
+          0% {
+            opacity: 0.2;
+            background: #18753c;
+          }
+          100% {
+            opacity: 1;
+            background: none;
+          }
+        }
+      `}
+    >
+      <div>
+        <span>Adresse</span>
+        <p className="fr-badge">
+          {situation['logement . adresse'].replace(/"/g, '')}
+        </p>
+      </div>
+      <div>
+        <span>Aléa argiles</span>
+        <p className="fr-badge">{data.alea_argiles || 'Nul'}</p>
+      </div>
+      {data.annee_construction && (
+        <div>
+          <span>Année de construction</span>
+          <p className="fr-badge">{data.annee_construction}</p>
+        </div>
+      )}
+      {data.nb_niveau && (
+        <div>
+          <span>Niveaux</span>
+          <p className="fr-badge">{data.nb_niveau}</p>
+        </div>
+      )}
+      <div>
+        <small
+          css={`
+            color: #aaa;
+          `}
+        >
+          Données issues du <a href="https://rnb.beta.gouv.fr/">RNB</a> et de la{' '}
+          <a href="https://bdnb.io/">BDNB</a>
+        </small>
+      </div>
+    </section>
+  )
+}
