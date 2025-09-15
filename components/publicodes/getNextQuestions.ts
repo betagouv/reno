a1eec0510be52da0c5eccae3c7010c2c93a6cf29
Mael <laem@kont.me>
2025-09-15 22:14:47 +0200

    1er algo potable pour le classement par chapitre

--- components/publicodes/getNextQuestions.ts
+++ components/publicodes/getNextQuestions.ts
@@ -32,15 +32,23 @@ export default function getNextQuestions(
         return chapterIndex === -1 ? Infinity : chapterIndex
       })(orderedEntries)
 
-  console.log('chapter', orderedByChapter)
+  console.log(
+    'chapter',
+    [...orderedByChapter].map(([k, v]) => k),
+  )
 
+  //TODO désactivé pour la v1 de RGA qui a plus besoin de catégories que d'ordre logique
+  /*
   const prio = questionsConfig.prioritaires || []
 
-  const artificialOrdered = sortBy(([k, v]) =>
-    prio.includes(k)
-      ? maxScore + [...prio].reverse().findIndex((kk) => kk === k) + 1
-      : v,
-  )(orderedByChapter).reverse()
+  const artificialOrdered = [
+    ...sortBy(([k, v]) =>
+      prio.includes(k)
+        ? maxScore + [...prio].reverse().findIndex((kk) => kk === k) + 1
+        : v,
+    )(orderedByChapter),
+  ].reverse()
+  */
 
   // Our "prioritaires" system would only retain questions that are included by publicodes in the missingVariables. Even if the model does not trigger question xyz, we may want manually trigger it in the config if it helps bypass other questions of the model, if these are more complex to anwser than e.g. using an API in the frontend to fill the questions.
   // It's easy in publicodes to make a variable depend on another (e.g. the department depend on a city question with "formule: city"), but this fails if the API question can be passed by the user : one rule should not depend on a formula referencing a question that can be "undefined" if the user passed it. Hence the following block of code
@@ -49,10 +57,14 @@ export default function getNextQuestions(
   const unansweredArtificialQuestions = (
     questionsConfig['préface'] || []
   ).filter((question) => !answeredQuestions.find((q) => q === question))
+
   const nextQuestions = [
     ...unansweredArtificialQuestions,
-    ...artificialOrdered.map(([k, v]) => k),
+    ...orderedByChapter
+      .map(([k, v]) => k)
+      .filter((q) => !unansweredArtificialQuestions.includes(q)),
   ]
 
+  console.log('chapter2', nextQuestions)
   return nextQuestions
 }
