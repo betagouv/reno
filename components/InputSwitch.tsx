39c7f2cbda8785946a86777e1154697a0f06bfb4
Mael <laem@kont.me>
2025-09-30 17:10:51 +0200

    Création d'un espace de nom pour les questions d'indemnisation

--- components/InputSwitch.tsx
+++ components/InputSwitch.tsx
@@ -70,7 +70,7 @@ export default function InputSwitch({
       'ménage . personnes',
       'logement . niveaux',
       'logement . âge',
-      'rga . montant indemnité',
+      'rga . indemnisé . montant indemnité',
     ].includes(currentQuestion),
     noButtons: [
       'projet . définition . travaux envisagés',
