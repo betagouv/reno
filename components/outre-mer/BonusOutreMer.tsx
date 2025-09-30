39ebca82aa5c82b448f572227fcb5273c3f2a934
Mael <laem@kont.me>
2025-09-30 17:48:47 +0200

    Icônes pour chaque DROM et première fiche correcte visuellement

--- components/outre-mer/BonusOutreMer.tsx
+++ components/outre-mer/BonusOutreMer.tsx
@@ -1,4 +1,8 @@
+import outreMerImage from '@/public/bonusOutreMer.svg'
+import Badge from '@codegouvfr/react-dsfr/Badge'
+import Image from 'next/image'
 import { formatValue } from 'publicodes'
+import { BlocAide } from '../UI'
 
 export default function BonusOutreMer({
   engine,
@@ -15,9 +19,53 @@ export default function BonusOutreMer({
     .setSituation(situation)
     .evaluate(bonusDottedName + ' . montant')
 
-  console.log('indigo', evaluation)
+  //TODO ne pas afficher si !nodeValue
+  const isEligible = true
 
-  return formatValue(evaluation)
+  const value = formatValue(evaluation)
+  console.log('indigo', evaluation, value)
 
-  return 'Bonus outre-mer'
+  const dispositif = engine.evaluate(
+    'gestes . outre-mer . dispositif',
+  ).nodeValue
+
+  return (
+    <BlocAide display="geste">
+      <div className="aide-header">
+        <OutreMerImage codeRégion="02" nom="Martinique" />
+        <div>
+          <h4 className="fr-m-0">Prime {dispositif}</h4>
+          <Badge noIcon severity={isEligible ? 'success' : 'default'}>
+            Prime de {value}
+          </Badge>
+        </div>
+      </div>
+    </BlocAide>
+  )
+}
+
+const OutreMerImage = ({ codeRégion, nom }) => {
+  return (
+    <div
+      css={`
+        img {
+          width: 3rem;
+          height: auto;
+        }
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-right: 1rem;
+      `}
+    >
+      <img
+        src={`/outre-mer/${codeRégion}.svg`}
+        width="10"
+        height="10"
+        alt={`Image représentant la form de ${nom}`}
+      />
+
+      <span>{nom}</span>
+    </div>
+  )
 }
