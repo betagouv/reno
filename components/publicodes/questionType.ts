53a891eaac7a061141d8cd4d50b8c8a500bb9e2a
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-10-16 10:56:16 +0200

    Réduction de la taille du champ copropriété . nombre de logement

--- components/publicodes/questionType.ts
+++ components/publicodes/questionType.ts
@@ -2,7 +2,7 @@ export default function questionType(evaluation, yamlRule) {
   if (yamlRule['par défaut'] && ['oui', 'non'].includes(yamlRule['par défaut']))
     return 'boolean'
   const value = evaluation.nodeValue
-  const ruleType = { number: 'number', string: 'text', undefined: 'text' }[
+  const ruleType = { number: 'number', string: 'text', undefined: 'number' }[
     typeof value
   ]
   return ruleType
