cf5e705fad78a006462955b239f0aa56af4a946b
Mael <laem@kont.me>
2025-10-28 16:46:15 +0100

    Les props dans Next maintenant sont async

--- app/simulation/page.tsx
+++ app/simulation/page.tsx
@@ -4,10 +4,11 @@ import { StartDsfrOnHydration } from '@/src/dsfr-bootstrap'
 import { PageBlock } from '@/components/UI'
 import simulationConfig from '/app/simulation/simulationConfig.yaml'
 
-export async function generateMetadata({ searchParams }) {
+export async function generateMetadata(props) {
+  const { objectif } = await props.searchParams
   return {
     title:
-      searchParams.objectif == 'etape'
+      objectif == 'etape'
         ? 'Guide - Comment obtenir vos aides'
         : 'Estimez vos aides à la rénovation énergétique',
     description: `Calculez les aides MaPrimeRénov' 2025 pour la rénovation de votre logement. MaPrimeRénov', les aides des fournisseurs d’énergie (CEE), primes "Coup de Pouce", éco-ptz, exonération de taxe foncière et dispositif Denormandie`,
