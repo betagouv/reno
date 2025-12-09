1a68505c88f99734f8bf60690d73953e7fed49e1
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-12-09 18:40:36 +0100

    Fusion des pages stats/stats internes

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -2,6 +2,8 @@ import { PageBlock } from '@/components/UI'
 import Statistiques from './Statistiques'
 import { Metadata } from 'next'
 import { StartDsfrOnHydration } from '@/src/dsfr-bootstrap'
+import StatistiquesInternes from './StatistiquesInternes'
+import Breadcrumb from '@codegouvfr/react-dsfr/Breadcrumb'
 
 const description = `Statistiques du site Mes Aides Réno.`
 
@@ -17,8 +19,17 @@ export default function Page() {
   return (
     <>
       <StartDsfrOnHydration />
+
       <PageBlock>
+        <Breadcrumb
+          currentPageLabel="Statistiques"
+          homeLinkProps={{
+            href: '/',
+          }}
+          segments={[]}
+        />
         <Statistiques />
+        <StatistiquesInternes />
       </PageBlock>
     </>
   )
