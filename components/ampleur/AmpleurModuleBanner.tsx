542e2032e4fdddb14ebcfdaae77ad1c16c2e8e4a
Morgan Merzouk <morgan.merzouk@gmail.com>
2025-12-10 15:24:58 +0100

    Supprimer la phrase dynamique "Vous avez déjà répondu à xx questions..."

--- components/ampleur/AmpleurModuleBanner.tsx
+++ components/ampleur/AmpleurModuleBanner.tsx
@@ -5,20 +5,11 @@ export default function AmpleurModuleBanner({ depuisModule, situation }) {
 
   return (
     <section>
-      <div>
-        <h2>
-          Simulation pour le bien situé
-          <City situation={situation} />
-          <DPE situation={situation} />
-        </h2>
-        <section>
-          <p>
-            <span className="fr-icon-success-line" aria-hidden="true"></span>{' '}
-            Vous avons déjà répondu à {Object.keys(situation).length} questions,
-            plus que quelques-unes pour calculer vos aides.
-          </p>
-        </section>
-      </div>
+      <h2>
+        Simulation pour le bien
+        <City situation={situation} />
+        <DPE situation={situation} />
+      </h2>
     </section>
   )
 }
@@ -27,7 +18,7 @@ const City = ({ situation }) => {
   const name = situation['logement . commune . nom']
   if (!name) return null
   const cleanName = name.replace(/"/g, '')
-  return <span> à {cleanName}</span>
+  return <span>situé à {cleanName}</span>
 }
 const DPE = ({ situation }) => {
   const value = situation['DPE . actuel']
